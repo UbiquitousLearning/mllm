b83b68bf0692ee99d86f5ce81c4746535738e1d6
Qiu Qichen <ruiqurm@gmail.com>
2023-09-23 06:42:00 +0000

    add memory test

--- src/memory/SystemMemoryManager.cpp
+++ src/memory/SystemMemoryManager.cpp
@@ -3,29 +3,29 @@
 #include <cassert>
 #include <cstdint>
 #include <cstdlib>
-
+#include <cstdio>
 namespace mllm {
 
-static inline void **alignPointer(void **ptr, size_t alignment) {
+static inline void **align(void **ptr, size_t alignment) {
     return (void **)((intptr_t)((unsigned char *)ptr + alignment - 1) & -alignment);
 }
 
 void SystemMemoryManager::Alloc(void **ptr, size_t size,size_t alignment){
     assert(size > 0);
-
-    void **origin = (void **)malloc(size + sizeof(void *) + alignment);
-    assert(origin != NULL);
+    // 分配一块内存，void*用来存储原始指针
+    void **origin = (void **)malloc(size + sizeof(void *) + alignment-1);
+    assert(origin != nullptr);
     if (!origin) {
-        *ptr = NULL;
+        *ptr = nullptr;
     }
-
-    void **aligned = alignPointer(origin + 1, alignment);
+    void **aligned = (void**)(((size_t)(origin) + sizeof(void*) +  alignment - 1) & (~(alignment - 1)));
+    // printf("origin = %p, align=%p\n",origin,aligned);
     aligned[-1]    = origin;
     *ptr = aligned;
 }
 
 void SystemMemoryManager::Free(void **ptr){
-    free(*ptr);
+    free(((void**)(*ptr))[-1]);
 }
 
 } // namespace mllm
