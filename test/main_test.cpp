6b1323c23d32920aa2d5d0602411ebfc9754dcf7
Yeeethan00 <yirj0809@gmail.com>
2023-09-27 17:38:04 +0800

    fix:Graph::setUp中input忘记alloc了

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -50,7 +50,7 @@ int main() {
      x = _Softmax(c, {x}, -1);
      */
 
-
+    /*
     // decoder blk sample
     Context *c = new Context();
     auto *in = _Input(c, {1, 1, 1, 1});
@@ -68,14 +68,14 @@ int main() {
     x = _Linear(c, {x}, 1, 1, false);
     x = _Linear(c, {x}, 1, 1, false);
     o = _Add(c, {o, x});
+    */
 
 
-    /*
     Context *c = new Context();
     auto *x = _Input(c, {1, 32, 1, 1});
     x = _Linear(c, {x}, 32, 16, false);
     x = _Softmax(c, {x}, 1);
-    */
+
 
     // display(c);
 
