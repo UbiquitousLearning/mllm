2ecc1e76f8d89733a0be1c550f5adccae23f4a5c
Yeeethan00 <yirj0809@gmail.com>
2023-09-28 14:59:59 +0800

    fix:_SelfAttention还存在BUG，TODO q_permute

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -72,7 +72,7 @@ int main() {
 
 
     Context *c = new Context();
-    auto *x = _Input(c, {1, 32, 1, 1});
+    auto *x = _Input(c, {1, 16, 1, 1});
     x = _Linear(c, {x}, 32, 16, false);
     x = _Softmax(c, {x}, 1);
 //    x = _SelfAttention(c, {x}, 16, 16);
