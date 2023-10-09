37c93c342d3bf73a239198345210768f2d03cbf5
oreomaker <zh002919@outlook.com>
2023-10-09 11:31:02 +0800

    chore: add android 64 build shell

---
+++ project/android/build_64.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cmake ../../../ \
+-DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
+-DCMAKE_BUILD_TYPE=Release \
+-DANDROID_ABI="arm64-v8a" \
+-DANDROID_STL=c++_static \
+-DNNAPI=ON \
+-DANDROID_NATIVE_API_LEVEL=android-27  \
+-DMNN_BUILD_FOR_ANDROID_COMMAND=true \
+-DNATIVE_LIBRARY_OUTPUT=. -DNATIVE_INCLUDE_OUTPUT=. $1 $2 $3
+
+make -j4
