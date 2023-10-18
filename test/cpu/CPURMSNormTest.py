d1d414f066a980d593b9f505d62c6dee9abc7c26
Lee <44310445+lx200916@users.noreply.github.com>
2023-10-18 20:27:04 +0800

    test: fix RMSNorm

--- test/cpu/CPURMSNormTest.py
+++ test/cpu/CPURMSNormTest.py
@@ -4,7 +4,7 @@ from TestUtils import TestBase
 
 
 class RMSNorm(torch.nn.Module):
-    def __init__(self, dim: int, eps: float = 1e-6):
+    def __init__(self, dim: int, eps: float = 1e-5):
         super().__init__()
         self.eps = eps
         self.weight = torch.nn.Parameter(torch.ones(dim))
