d9a5443a03d2e3c430c17d58922e63c65d58ea83
Lee <44310445+lx200916@users.noreply.github.com>
2023-11-08 23:27:50 +0800

    feat: Add Mobile CI.

---
+++ tools/ci/mobile_test.py
@@ -0,0 +1,34 @@
+import subprocess
+import sys
+import json
+
+Device_List = []
+if __name__ == "__main__":
+    output = subprocess.getoutput("adb --json devices -l", shell=True)
+    devices = json.loads(output)
+    # [{"serial":"6fd28b00","state":"Device","product":"syberia_oneplus6","model":"ONEPLUS_A6000","device":"oneplus6","devpath":"usb:3-4","transport_id":1}]
+    Device_List = list(
+        filter(lambda device: device.get("state", None) == "Device", devices)
+    )
+    for device in Device_List:
+        print(device)
+        subprocess.run(
+            f"adb -s {device.get('serial','')} shell rm -rf /data/local/tmp/mllm"
+        )
+        subprocess.run(
+            f"adb -s {device.get('serial','')} push ./bin/ /data/local/tmp/mllm/"
+        )
+        subprocess.run(
+            f"adb -s {device.get('serial','')} push ./bin-arm/ /data/local/tmp/mllm/"
+        )
+        subprocess.run(
+            f"adb -s {device.get('serial','')} shell chmod +x /data/local/tmp/mllm/MLLM_TEST"
+        )
+        subprocess.run(
+            f"adb -s {device.get('serial','')} shell \"cd /data/local/tmp/mllm && /data/local/tmp/mllm/MLLM_TEST --gtest_filter=* --gtest_output=json\"",
+            #    Output the stdout
+            stdout=subprocess.PIPE,
+        )
+        subprocess.run(
+            f"adb -s {device.get('serial','')} /data/local/tmp/test_detail.json ./adb_{device.get('device','')}.json"
+        )
--- tools/ci/report.py
+++ tools/ci/report.py
@@ -5,27 +5,52 @@ from notify import Feishu, PRComment
 
 commit_user = os.environ.get("GITHUB_ACTOR", "unknown")
 commit_message = os.environ.get("COMMIT_MESSAGE", "unknown")
-template = f'''\n 👶 {commit_user} \n 📝 {commit_message} \n 📦 {os.environ.get("RUNNER_NAME", "unknown")}_{os.environ.get("RUNNER_OS", 'unknown')}_{os.environ.get("RUNNER_ARCH", 'unknown')} \n 🚀 {os.environ.get("GITHUB_SERVER_URL", )}/{os.environ.get("GITHUB_REPOSITORY", )}/actions/runs/{os.environ.get("GITHUB_RUN_ID", )} \n'''
+template = f"""\n 👶 {commit_user} \n 📝 {commit_message} 
+ 📦 {os.environ.get("RUNNER_NAME", "unknown")}_{os.environ.get("RUNNER_OS", 'unknown')}_{
+     os.environ.get("RUNNER_ARCH",
+ 'unknown')} \n 🚀 {os.environ.get("GITHUB_SERVER_URL", )}/{os.environ.get("GITHUB_REPOSITORY",
+     )}/actions/runs/{os.environ.get("GITHUB_RUN_ID", )} \n"""  # noqa: '''
 
-if __name__ == '__main__':
+
+def mobile_test():
+    fs = Feishu()
+    for file in os.listdir("./"):
+        if file.startswith("adb_") and file.endswith(".json"):
+            # FileName: adb_<serial>.json
+            serial = file[4:-5]
+            template = f"""\n 👶 {commit_user} \n 📝 {commit_message} \n 📦 {serial} \n"""
+            handle_reports(filename=file, template=template)
+
+
+def handle_reports(filename: str, template: str):
+    report = json.load(open(filename))
+    test_nums = report.get("tests", 0) - report.get("disabled", 0)
+    test_failures = report.get("failures", 0) + report.get("errors", 0)
+    test_success = test_nums - test_failures
+    if test_failures == 0:
+        print(" ✅ Test Passed!")
+        exit(0)
+    else:
+        message = (
+            template + f" ❌ Test Failed! \n 📊 {test_success}/{test_nums} \n Details:\n"
+        )
+        for ts in report.get("testsuites", []):
+            for test in ts.get("testsuite", []):
+                if len(test.get("failures", [])) > 0:
+                    failures = test.get("failures", [])
+                    message += f"   ❌ {test.get('classname', 'unknown')}.{test.get('name', 'unknown')} \n"
+        fs.notify(message)
+        PRComment().notify(message)
+
+
+if __name__ == "__main__":
+    is_mobile = os.environ.get("IS_MOBILE", False)
+    if is_mobile:
+        mobile_test()
+        exit(0)
     fs = Feishu()
     if not os.path.exists("test_detail.json"):
         print("No Test Report Found!")
         fs.notify(template + "⚠ Test Ended but No Test Report Found!")
     else:
-        report = json.load(open("test_detail.json"))
-        test_nums = report.get("tests", 0) - report.get("disabled", 0)
-        test_failures = report.get("failures", 0) + report.get("errors", 0)
-        test_success = test_nums - test_failures
-        if test_failures == 0:
-            print(" ✅ Test Passed!")
-            exit(0)
-        else:
-            message = template + f" ❌ Test Failed! \n 📊 {test_success}/{test_nums} \n Details:\n"
-            for ts in report.get("testsuites", []):
-                for test in ts.get("testsuite", []):
-                    if len(test.get("failures", [])) > 0:
-                        failures = test.get("failures", [])
-                        message += f"   ❌ {test.get('classname', 'unknown')}.{test.get('name', 'unknown')} \n"
-            fs.notify(message)
-            PRComment().notify(message)
+        handle_reports("test_detail.json", template=template)
