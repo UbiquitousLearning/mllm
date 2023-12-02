fa24f5b98854a1e58a4f1213fbff894b3761fa9f
Lee <44310445+lx200916@users.noreply.github.com>
2023-12-02 13:20:51 +0800

    feat: Add Image Read Function.

--- demo/main_fuyu.cpp
+++ demo/main_fuyu.cpp
@@ -1,26 +1,22 @@
-#include "CImg/CImg.h"
 #include <vector>
 #include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "imageHelper/stb_image.h"
 
-using namespace cimg_library;
 using namespace std;
 
 int main() {
-    CImg<unsigned char> image("./lena.jpg");
-    vector<float> pixels;
-
-    cimg_forXY(image, x, y) {
-        // 将像素值转换为浮点数并存储到vector中
-        pixels.push_back(static_cast<float>(image(x, y, 0, 0)));
-        pixels.push_back(static_cast<float>(image(x, y, 0, 1)));
-        pixels.push_back(static_cast<float>(image(x, y, 0, 2)));
+    int width, height, channel;
+    unsigned char *data = stbi_load("test.jpg", &width, &height, &channel, 0);
+    if (data == nullptr) {
+        cout << "load image failed" << endl;
+        return -1;
     }
-
-    // 打印vector中的值
-    for (const auto &pixel : pixels) {
-        cout << pixel << ' ';
+    cout << "width: " << width << " height: " << height << " channel: " << channel << endl;
+    vector<float> data_f32(width * height * channel);
+    for (int i = 0; i < width * height * channel; i++) {
+        data_f32[i] = data[i] / 255.0;
     }
-    cout << endl;
-
+    stbi_image_free(data);
     return 0;
 }
