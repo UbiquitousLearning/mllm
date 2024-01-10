970c42e399aeb700ed35e362921da7f980bf7b11
xudaliang <xudaliang@pku.edu.cn>
2024-01-10 07:26:34 +0000

    fix: qnn rms norm output name

--- src/backends/QNN/op/QNNRMSNorm.cpp
+++ src/backends/QNN/op/QNNRMSNorm.cpp
@@ -17,34 +17,33 @@ ErrorCode QNNRMSNorm::reshape(vector<shared_ptr<Tensor>> inputs, vector<shared_p
 }
 
 ErrorCode QNNRMSNorm::setUp(vector<shared_ptr<Tensor>> inputs, vector<shared_ptr<Tensor>> outputs) {
-    auto inName = inputs[0]->name();
-    auto weightName = weight_.name();
     uint32_t dimWeight[4] = {(uint32_t)normSize_};
-    qnnBackend_->modelAddTensor(weightName.c_str(), (Qnn_Tensor_t){
-                                                        .version = QNN_TENSOR_VERSION_1,
-                                                        {.v1 = {
-                                                             .id = 0,
-                                                             .name = weightName.c_str(),
-                                                             .type = QNN_TENSOR_TYPE_STATIC,
-                                                             .dataFormat = QNN_TENSOR_DATA_FORMAT_FLAT_BUFFER,
-                                                             .dataType = QNN_DATATYPE_FLOAT_32,
-                                                             .quantizeParams = {QNN_DEFINITION_UNDEFINED,
-                                                                                QNN_QUANTIZATION_ENCODING_UNDEFINED,
-                                                                                {.scaleOffsetEncoding = {.scale = 0.0000000000000000f, .offset = 0}}},
-                                                             .rank = 1,
-                                                             .dimensions = dimWeight,
-                                                             .memType = QNN_TENSORMEMTYPE_RAW,
-                                                             {.clientBuf = {.data = weight_.hostPtr<void>(),
-                                                                            .dataSize = static_cast<uint32_t>(weight_.cntSize())}}}}});
-    auto outString = outputs[0]->name();
+    qnnBackend_->modelAddTensor(weight_.name(), (Qnn_Tensor_t){
+                                                    .version = QNN_TENSOR_VERSION_1,
+                                                    {.v1 = {
+                                                         .id = 0,
+                                                         .name = weight_.name().c_str(),
+                                                         .type = QNN_TENSOR_TYPE_STATIC,
+                                                         .dataFormat = QNN_TENSOR_DATA_FORMAT_FLAT_BUFFER,
+                                                         .dataType = QNN_DATATYPE_FLOAT_32,
+                                                         .quantizeParams = {QNN_DEFINITION_UNDEFINED,
+                                                                            QNN_QUANTIZATION_ENCODING_UNDEFINED,
+                                                                            {.scaleOffsetEncoding = {.scale = 0.0000000000000000f, .offset = 0}}},
+                                                         .rank = 1,
+                                                         .dimensions = dimWeight,
+                                                         .memType = QNN_TENSORMEMTYPE_RAW,
+                                                         {.clientBuf = {.data = weight_.hostPtr<void>(),
+                                                                        .dataSize = static_cast<uint32_t>(weight_.cntSize())}}}}});
+
     uint32_t dimOut[] = {(uint32_t)outputs[0]->batch(), (uint32_t)outputs[0]->head(), (uint32_t)outputs[0]->sequence(), (uint32_t)outputs[0]->dimension()};
+    auto outName = outputs[0]->name();
     vector<Qnn_Tensor_t>
         out = {
             (Qnn_Tensor_t){
                 .version = QNN_TENSOR_VERSION_1,
                 {.v1 = {
                      .id = 0,
-                     .name = outString.c_str(),
+                     .name = outName.c_str(),
                      .type = getOutputTensorType(outputs[0]),
                      .dataFormat = QNN_TENSOR_DATA_FORMAT_FLAT_BUFFER,
                      .dataType = QNN_DATATYPE_FLOAT_32,
@@ -56,7 +55,7 @@ ErrorCode QNNRMSNorm::setUp(vector<shared_ptr<Tensor>> inputs, vector<shared_ptr
                      .memType = QNN_TENSORMEMTYPE_RAW,
                      {.clientBuf = {.data = nullptr,
                                     .dataSize = 0}}}}}};
-    return graphAddNode(name(), "RMSNorm", {inName.c_str(), weightName.c_str()}, out, {}, "LLaMAPackage");
+    return graphAddNode(name(), "RMSNorm", {inputs[0]->name(), weight_.name()}, out, {}, "LLaMAPackage");
 }
 
 ErrorCode QNNRMSNorm::load(AbstructLoader &loader) {
