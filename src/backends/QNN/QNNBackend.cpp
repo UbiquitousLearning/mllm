ba1612e6e91b516dd90278579fa4f575993917d0
xudaliang <xudaliang@pku.edu.cn>
2024-01-11 01:18:23 +0800

    fix : LLaMA op package loading in android

--- src/backends/QNN/QNNBackend.cpp
+++ src/backends/QNN/QNNBackend.cpp
@@ -81,7 +81,7 @@ QNNBackend::QNNBackend(shared_ptr<MemoryManager> mm) : Backend(mm) {
 #elifdef QNN_ARM
     std::string backEndPath = "libQnnHtp.so";
     std::string inputListPaths = "./qnn/input-list.txt";
-    std::string opPackagePaths = "./qnn/llama-op-hexagon75/libQnnLLaMAPackage.so:LLaMAPackageInterfaceProvider";
+    std::string opPackagePaths = "libQnnLLaMAPackage_CPU.so:LLaMAPackageInterfaceProvider:CPU,libQnnLLaMAPackage_HTP.so:LLaMAPackageInterfaceProvider:HTP";
 #else
     std::string modelPath = "/mllm/qualcomm_ai_engine_direct_new/examples/QNN/example_libs/x86_64-linux-clang/libqnn_model_float.so";
     std::string backEndPath = "/mllm/qualcomm_ai_engine_direct_2.18/lib/x86_64-linux-clang/libQnnHtp.so";
@@ -90,7 +90,7 @@ QNNBackend::QNNBackend(shared_ptr<MemoryManager> mm) : Backend(mm) {
 #endif
 
     // TODO: make these configuable
-    m_debug = false; // when set true, NATIVE tensor will be regared as APP_READ tensor
+    m_debug = true; // when set true, NATIVE tensor will be regared as APP_READ tensor
     m_outputDataType = iotensor::OutputDataType::FLOAT_ONLY;
     m_inputDataType = iotensor::InputDataType::FLOAT;
     m_profilingLevel = ProfilingLevel::OFF;
