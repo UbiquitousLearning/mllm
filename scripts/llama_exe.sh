2c71f22859e1eef6ad4edf2fa7ecf762e85319cc
yirongjie <yirj0809@gmail.com>
2024-01-13 17:21:33 +0800

    doc: fix scripts

--- scripts/llama_exe.sh
+++ scripts/llama_exe.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo "You need to push models to phone first: /data/local/tmp/mllm/models"
+#echo "You need to push models to phone first: /data/local/tmp/mllm/models"
 
 adb mkdir /data/local/tmp/mllm
 adb mkdir /data/local/tmp/mllm/bin
 adb mkdir /data/local/tmp/mllm/models
-adb push ./libomp.so /data/local/tmp/mllm/bin
+adb mkdir /data/local/tmp/mllm/vocab
+adb push ../vocab/llama_vocab.mllm /data/local/tmp/mllm/vocab
 adb push ../bin-arm/main_llama /data/local/tmp/mllm/bin
 adb push ../vocab/vocab.mllm /data/local/tmp/mllm/bin
 adb push ../models/llama-2-7b-chat-q4_k.mllm /data/local/tmp/mllm/models
@@ -13,5 +14,4 @@ if [ $? -ne 0 ]; then
     echo "adb push failed"
     exit 1
 fi
-adb shell export LD_LIBRARY_PATH=/data/local/tmp/mllm/bin
 adb shell ./data/local/tmp/mllm/main_llama
