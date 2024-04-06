413bd62c8dfd2438ea8f10f898327438b154fc10
chenghuaWang <2923277184@qq.com>
2024-04-06 14:57:37 +0800

    feat: Split any length Support / Gemma modeling

---
+++ examples/demo_gemma.cpp
@@ -0,0 +1,63 @@
+/**
+ * @file demo_gemma.cpp
+ * @author Chenghua Wang (chenghua.wang@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-04-06
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#include "cmdline.h"
+#include "models/gemma/configuration_gemma.hpp"
+#include "models/gemma/modeling_gemma.hpp"
+#include "models/gemma/tokenization_gemma.hpp"
+#include "processor/PostProcess.hpp"
+
+using namespace mllm;
+
+int main(int argc, char **argv) {
+    cmdline::parser cmdParser;
+    cmdParser.add<string>("vocab", 'v', "specify mllm tokenizer model path", false, "../vocab/gemma/gemma_vocab.mllm");
+    cmdParser.add<string>("model", 'm', "specify mllm model path", false, "../models/gemma/gemma-2b-q4_k.mllm");
+    cmdParser.add<int>("limits", 'l', "max KV cache size", false, 400);
+    cmdParser.add<int>("thread", 't', "num of threads", false, 4);
+    cmdParser.parse_check(argc, argv);
+
+    string vocab_path = cmdParser.get<string>("vocab");
+    string model_path = cmdParser.get<string>("model");
+    int tokens_limit = cmdParser.get<int>("limits");
+    CPUBackend::cpu_threads = cmdParser.get<int>("thread");
+
+    auto tokenizer = GemmaTokenizer(vocab_path);
+
+    GemmaConfig config(tokens_limit, "2B");
+    auto model = GemmaForCausalLM(config);
+    model.load(model_path);
+
+    vector<string> in_strs = {
+        " Hello, who are you?",
+        " What can you do?",
+        "Please introduce Beijing University of Posts and Telecommunications."};
+
+    for (int i = 0; i < in_strs.size(); ++i) {
+        auto in_str = in_strs[i];
+        auto input_tensor = tokenizer.tokenize(in_str, i);
+        std::cout << "[Q] " << in_str << std::endl;
+        std::cout << "[A] " << std::flush;
+        for (int step = 0; step < 100; step++) {
+            auto result = model({input_tensor});
+            auto outputs = tokenizer.detokenize(result[0]);
+            auto out_string = outputs.first;
+            auto out_token = outputs.second;
+            if (out_token == 2) {
+                break;
+            }
+            std::cout << out_string << std::flush;
+            chatPostProcessing(out_token, input_tensor, {});
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
--- src/models/gemma/modeling_gemma.hpp
+++ src/models/gemma/modeling_gemma.hpp
@@ -11,10 +11,12 @@
 #ifndef MODELING_GEMMA_HPP
 #define MODELING_GEMMA_HPP
 
+#include "Backend.hpp"
 #include "Layer.hpp"
 #include "Module.hpp"
 #include "configuration_gemma.hpp"
 #include "models/transformer/modeling_transformer.hpp"
+#include <cmath>
 
 using namespace mllm;
 
@@ -47,25 +49,126 @@ private:
     Layer gelu; ///< F.gelu(gate, approximate="tanh")
 };
 
+///< gemma-2B use MQA while 7B use MHA
 class GemmaAttention final : public Module {
 public:
     GemmaAttention() = default;
-    GemmaAttention(int hidden_size, int num_heads, int num_kv_heads, int head_dims, int cache_limit, const GemmaNameConfig &names, const string &base_name) {
+    GemmaAttention(int hidden_size, int num_heads, int num_kv_heads, int head_dim, int cache_limit, const GemmaNameConfig &names, const string &base_name) :
+        hidden_size(hidden_size), num_heads(num_heads), num_kv_heads(num_kv_heads), head_dim(head_dim) {
+        assert(num_heads % num_kv_heads == 0 && "When using MQA/GQA. num_heads mod num_kv_heads should 0");
+
+        // for MQA and GQA
+        num_queries_per_kv = num_heads / num_kv_heads;
+        q_size = num_heads * head_dim;
+        kv_size = num_kv_heads * head_dim;
+
+        // scaling
+        scaling = 1.f / std::sqrt(head_dim);
+
+        // init layers
+        qkv_proj = Linear(hidden_size, (num_heads + 2 * num_kv_heads) * head_dim, false, base_name + names._qkv_proj_name);
+        qkv_split = Split({q_size, kv_size, kv_size}, Chl::DIMENSION, base_name + names._qkv_proj_name + ".split");
+        o_proj = Linear(num_heads * head_dim, hidden_size, false, base_name + names._o_proj_name);
+        // FIXME RoPEType::HFHUBROPE
+        q_rope = RoPE(RoPEType::HFHUBROPE, base_name + "q_rope");
+        k_rope = RoPE(RoPEType::HFHUBROPE, base_name + "k_rope");
+        if (cache_limit > 0) {
+            k_cache = KVCache(num_heads / num_kv_heads, cache_limit, base_name + "k_cache");
+            v_cache = KVCache(num_heads / num_kv_heads, cache_limit, base_name + "v_cache");
+        }
+        mask = Causalmask(base_name + "mask");
+        softmax = Softmax(DIMENSION, base_name + "softmax");
     }
 
     std::vector<Tensor> Forward(std::vector<Tensor> inputs, std::vector<std::any> args) override {
-        return {attention({inputs[0], inputs[0], inputs[0]})[0]};
+        auto hidden_sates = inputs[0];
+        auto hidden_sates_shape = hidden_sates.shape();
+        auto batch_size = hidden_sates_shape[0];
+        auto input_size = hidden_sates_shape[1];
+
+        auto qkv = qkv_proj(hidden_sates);
+        auto qkv_sp = qkv_split(qkv);
+        auto xq = qkv_sp[0];
+        auto xk = qkv_sp[1];
+        auto xv = qkv_sp[2];
+
+        xq = xq.view(batch_size, -1, num_heads, head_dim);
+        xk = xk.view(batch_size, -1, num_kv_heads, head_dim);
+        xv = xv.view(batch_size, -1, num_kv_heads, head_dim);
+
+        // position embedding
+        xq = q_rope(xq);
+        xk = k_rope(xk);
+
+        // kv cache
+        auto key = k_cache(xk);
+        auto value = v_cache(xv);
+
+        // repeat
+        if (num_kv_heads != num_heads) {
+            std::vector<Tensor> _key_repeat, _value_repeat;
+            for (int i = 0; i < num_queries_per_kv; ++i) _key_repeat.push_back(key);
+            for (int i = 0; i < num_queries_per_kv; ++i) _value_repeat.push_back(value);
+            key = Tensor::cat(_key_repeat, /*dims*/ Chl::SEQUENCE);
+            value = Tensor::cat(_value_repeat, /*dims*/ Chl::SEQUENCE);
+        }
+
+        // [batch, head, seq, dim]
+        auto q = xq.transpose(Chl::HEAD, Chl::SEQUENCE);
+        auto k = key.transpose(Chl::HEAD, Chl::SEQUENCE);
+        auto v = value.transpose(Chl::HEAD, Chl::SEQUENCE);
+
+        // [batch_size, n_local_heads, input_len, max_seq_len]
+        auto scores = Tensor::mm(q, k.transpose(Chl::SEQUENCE, Chl::DIMENSION)) * scaling;
+        scores = mask(scores);
+        scores = softmax(scores);
+
+        // output
+        auto output = Tensor::mm(scores, v);
+        // FIXME fix vie size.
+        output = output.transpose(Chl::HEAD, Chl::SEQUENCE).view(batch_size, input_size, -1, -1);
+        output = o_proj(output);
+        return {output};
     }
 
 private:
-    MultiHeadAttention attention;
+    int hidden_size;
+    int num_heads;
+    int num_kv_heads;
+    int head_dim;
+
+    int num_queries_per_kv;
+    int q_size;
+    int kv_size;
+    float scaling;
+
+    // layers
+    Layer qkv_proj;
+    Layer o_proj;
+    Split qkv_split;
+    Layer q_rope;
+    Layer k_rope;
+    Layer k_cache;
+    Layer v_cache;
+    Layer mask;
+    Layer softmax;
 };
 
 class GemmaDecoder final : public Module {
 public:
     GemmaDecoder() = default;
-    GemmaDecoder(const GemmaNameConfig &names, const string &base_name) {
-        // TODO
+    GemmaDecoder(const GemmaConfig &config, const GemmaNameConfig &names, const string &base_name) {
+        self_atten = GemmaAttention(
+            config.hidden_size,
+            config.num_attention_heads,
+            config.num_key_value_heads,
+            config.head_dim,
+            config.cache_limit,
+            names,
+            base_name);
+        mlp = GemmaMLP(config.hidden_size, config.intermediate_size, names, base_name);
+        input_layernorm = RMSNorm(config.hidden_size, config.rms_norm_eps, base_name + names._attn_norm_name);
+        post_attention_layernorm = RMSNorm(config.hidden_size, config.rms_norm_eps, base_name + names._ffn_norm_name);
     }
 
     std::vector<Tensor> Forward(std::vector<Tensor> inputs, std::vector<std::any> args) override {
@@ -93,6 +196,12 @@ private:
 
 class GemmaModle final : public Module {
 public:
+    GemmaModle() = default;
+    GemmaModle(const GemmaConfig &config, const GemmaNameConfig &names, const string &base_name) {
+        for (int i = 0; i < config.num_hidden_layers; ++i) layers.push_back(GemmaDecoder(config, names, base_name));
+        norm = RMSNorm(config.hidden_size, config.rms_norm_eps, base_name + names.post_norm_name);
+    }
+
     std::vector<Tensor> Forward(std::vector<Tensor> inputs, std::vector<std::any> args) override {
         auto x = inputs[0];
         for (auto &layer : layers) {
@@ -109,7 +218,21 @@ private:
 
 class GemmaForCausalLM final : public Module {
 public:
+    GemmaForCausalLM(GemmaConfig &config) {
+        auto names = config.names_config;
+        embedding = Embedding(config.vocab_size, config.hidden_size, names.token_embd_name);
+        model = GemmaModle(config, names, names.blk_name);
+    }
+
+    std::vector<Tensor> Forward(std::vector<Tensor> inputs, std::vector<std::any> args) override {
+        auto x = embedding(inputs[0]);
+        auto outputs = model({x});
+        return outputs;
+    }
+
 private:
+    Layer embedding;
+    GemmaModle model;
 };
 
 #endif //! MODELING_GEMMA_HPP
