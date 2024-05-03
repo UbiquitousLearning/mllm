faea00e2efe283feeec29861fa1e37d397c8a601
chenghuaWang <2923277184@qq.com>
2024-05-03 14:29:30 +0800

    feat: tokenizer of qwen, done.

--- examples/demo_qwen.cpp
+++ examples/demo_qwen.cpp
@@ -18,40 +18,42 @@ using namespace mllm;
 int main(int argc, char **argv) {
     cmdline::parser cmdParser;
     cmdParser.add<string>("vocab", 'v', "specify mllm tokenizer model path", false, "../vocab/qwen_vocab.mllm");
+    cmdParser.add<string>("merge", 'e', "specify mllm merge file path", false, "../vocab/qwen_merges.txt");
     cmdParser.add<string>("model", 'm', "specify mllm model path", false, "../models/qwen/qwen_q4_k.mllm");
     cmdParser.add<int>("limits", 'l', "max KV cache size", false, 400);
     cmdParser.add<int>("thread", 't', "num of threads", false, 4);
     cmdParser.parse_check(argc, argv);
 
     string vocab_path = cmdParser.get<string>("vocab");
+    string merge_path = cmdParser.get<string>("merge");
     string model_path = cmdParser.get<string>("model");
     int tokens_limit = cmdParser.get<int>("limits");
     CPUBackend::cpu_threads = cmdParser.get<int>("thread");
 
-    auto tokenizer = QWenTokenizer(vocab_path);
-    // QWenConfig config(tokens_limit, "0.5B", RoPEType::HFHUBROPE);
-    // auto model = QWenForCausalLM(config);
-    // model.load(model_path);
+    auto tokenizer = QWenTokenizer(vocab_path, merge_path);
+    QWenConfig config(tokens_limit, "0.5B", RoPEType::HFHUBROPE);
+    auto model = QWenForCausalLM(config);
+    model.load(model_path);
 
     vector<string> in_strs = {
-        "Hello world",
-        " Hello world",
+        " Hello, who are you?",
+        " What can you do?",
+        "Please introduce Beijing University of Posts and Telecommunications.",
     };
 
     for (int i = 0; i < in_strs.size(); ++i) {
         auto in_str = in_strs[i];
         auto input_tensor = tokenizer.tokenize(in_str, i);
-        input_tensor.printData<float>();
-        // std::cout << "[Q] " << in_str << std::endl;
-        // std::cout << "[A] " << std::flush;
-        // for (int step = 0; step < 100; step++) {
-        //     auto result = model({input_tensor});
-        //     auto outputs = tokenizer.detokenize(result[0]);
-        //     auto out_string = outputs.first;
-        //     auto out_token = outputs.second;
-        //     std::cout << out_string << std::flush;
-        //     chatPostProcessing(out_token, input_tensor, {});
-        // }
-        // printf("\n");
+        std::cout << "[Q] " << in_str << std::endl;
+        std::cout << "[A] " << std::flush;
+        for (int step = 0; step < 100; step++) {
+            auto result = model({input_tensor});
+            auto outputs = tokenizer.detokenize(result[0]);
+            auto out_string = outputs.first;
+            auto out_token = outputs.second;
+            std::cout << out_string << std::flush;
+            chatPostProcessing(out_token, input_tensor, {});
+        }
+        printf("\n");
     }
 }
--- src/models/qwen/tokenization_qwen.hpp
+++ src/models/qwen/tokenization_qwen.hpp
@@ -13,9 +13,9 @@
 // regex
 #include <re2/re2.h>
 
+#include "tokenizers/BPE/Bpe.hpp"
 #include "tokenizers/Tokenizer.hpp"
 #include <algorithm>
-#include <optional>
 #include <unordered_map>
 
 // unicode
@@ -25,7 +25,9 @@
 
 using namespace mllm;
 
-namespace mllm {
+#define UTF8(x) any_to_utf8(x)
+#define CHR(x) __chr(x)
+#define ORD(x) __ord(x)
 
 static std::string any_to_utf8(std::string s) {
     icu::UnicodeString us(s.c_str());
@@ -34,276 +36,69 @@ static std::string any_to_utf8(std::string s) {
     return utf8;
 }
 
-static auto _byte_pair_merge(
-    const std::string &piece,
-    const std::unordered_map<std::string, int> &ranks,
-    std::function<int(int, int)> func) -> std::vector<int> {
-    std::vector<std::pair<int, int>> parts;
-    parts.reserve(piece.size() + 1);
-    for (auto idx = 0U; idx < piece.size() + 1; ++idx) {
-        parts.emplace_back(idx, std::numeric_limits<int>::max());
-    }
-
-    auto get_rank = [&piece, &ranks](
-                        const std::vector<std::pair<int, int>> &parts,
-                        int start_idx,
-                        int skip) -> std::optional<int> {
-        if (start_idx + skip + 2 < parts.size()) {
-            auto s = parts[start_idx].first;
-            auto e = parts[start_idx + skip + 2].first;
-            auto key = piece.substr(s, e - s);
-            auto iter = ranks.find(key);
-            if (iter != ranks.end()) {
-                return iter->second;
-            }
-        }
-        return std::nullopt;
-    };
-
-    for (auto i = 0U; i < parts.size() - 2; ++i) {
-        auto rank = get_rank(parts, i, 0);
-        if (rank) {
-            assert(*rank != std::numeric_limits<int>::max());
-            parts[i].second = *rank;
-        }
-    }
-
-    while (true) {
-        if (parts.size() == 1) break;
-
-        auto min_rank = std::make_pair<int, int>(std::numeric_limits<int>::max(), 0);
-        for (auto i = 0U; i < parts.size() - 1; ++i) {
-            auto rank = parts[i].second;
-            if (rank < min_rank.first) {
-                min_rank = {rank, i};
-            }
-        }
-
-        if (min_rank.first != std::numeric_limits<int>::max()) {
-            auto i = min_rank.second;
-            auto rank = get_rank(parts, i, 1);
-            if (rank) {
-                parts[i].second = *rank;
-            } else {
-                parts[i].second = std::numeric_limits<int>::max();
-            }
-            if (i > 0) {
-                auto rank = get_rank(parts, i - 1, 1);
-                if (rank) {
-                    parts[i - 1].second = *rank;
-                } else {
-                    parts[i - 1].second = std::numeric_limits<int>::max();
-                }
-            }
-
-            parts.erase(parts.begin() + (i + 1));
-        } else {
-            break;
-        }
-    }
-    std::vector<int> out;
-    out.reserve(parts.size() - 1);
-    for (auto i = 0U; i < parts.size() - 1; ++i) {
-        out.push_back(func(parts[i].first, parts[i + 1].first));
-    }
-    return out;
+static std::string __chr(int v) {
+    icu::UnicodeString us(v);
+    std::string utf8;
+    us.toUTF8String(utf8);
+    return utf8;
 }
 
-static auto byte_pair_encode(
-    const std::string &piece,
-    const std::unordered_map<std::string, int> &ranks) -> std::vector<int> {
-    if (piece.size() == 1) {
-        return {ranks.at(piece)};
-    }
-
-    auto func = [&piece, &ranks](int start, int stop) -> int {
-        std::string key = piece.substr(start, stop - start);
-
-        // key to utf-8
-        auto nkey = any_to_utf8(key);
-
-        return ranks.at("");
-    };
-
-    return _byte_pair_merge(piece, ranks, func);
+static std::vector<int> __ord(std::string v) {
+    std::vector<int> ret;
+    icu::UnicodeString us(v.c_str());
+    for (auto i = 0; i < us.length(); ++i) ret.emplace_back(us[i]);
+    return ret;
 }
-
-class TikTokenizer final : public Tokenizer {
-public:
-    explicit TikTokenizer(const std::string &vocab_file, const std::string &pattern) :
-        Tokenizer(vocab_file) {
-        regex_ = std::make_unique<re2::RE2>("(" + pattern + ")");
-    }
-
-    auto _encode_ordinary_native(const std::string &text) const -> std::vector<int> {
-        std::vector<int> ret;
-        re2::StringPiece input(text);
-
-        std::string piece;
-        while (re2::RE2::FindAndConsume(&input, *regex_, &piece)) {
-            auto iter = encoder_.find(piece);
-            if (iter != encoder_.end()) {
-                ret.push_back(iter->second);
-                continue;
-            }
-            auto tokens = byte_pair_encode(piece, encoder_);
-            ret.insert(ret.end(), tokens.begin(), tokens.end());
-        }
-        return ret;
-    }
-
-    auto encode_ordinary(const std::string &text) const -> std::vector<int> {
-        return _encode_ordinary_native(text);
-    }
-
-    auto encode(const std::string &text) const -> std::vector<int> {
-        return _encode_native(text, special_tokens_encoder).first;
-    }
-
-    auto _encode_native(
-        const std::string &text,
-        const std::unordered_map<std::string, int> &allowed_special) const -> std::pair<std::vector<int>, int> {
-        std::vector<int> ret;
-        int last_piece_token_len = 0;
-        re2::StringPiece input(text);
-
-        while (true) {
-            auto [special, sub_input] = split_with_allowed_special_token(input, allowed_special);
-            std::string piece;
-            while (re2::RE2::FindAndConsume(&sub_input, *regex_, &piece)) {
-                auto iter = encoder_.find(piece);
-                if (iter != encoder_.end()) {
-                    last_piece_token_len = 1;
-                    ret.push_back(iter->second);
-                    continue;
-                }
-                auto tokens = byte_pair_encode(piece, encoder_);
-                last_piece_token_len = tokens.size();
-                ret.insert(ret.end(), tokens.begin(), tokens.end());
-            }
-
-            if (special) {
-                int token = special_tokens_encoder.at(*special);
-                ret.push_back(token);
-                last_piece_token_len = 0;
-            } else {
-                break;
-            }
-        }
-
-        return {ret, last_piece_token_len};
-    }
-
-    auto split_with_allowed_special_token(
-        re2::StringPiece &input,
-        const std::unordered_map<std::string, int> &allowed_special) const -> std::pair<std::optional<std::string>, re2::StringPiece> {
-        if (special_regex_ == nullptr) return {std::nullopt, input};
-
-        auto start = input.begin();
-        std::string special;
-        while (true) {
-            if (!re2::RE2::FindAndConsume(&input, *special_regex_, &special)) {
-                break;
-            }
-
-            if (allowed_special.count(special) == 1) {
-                return {std::move(special), re2::StringPiece(start, input.begin() - start - special.size())};
-            }
-        }
-
-        return {std::nullopt, input};
-    }
-
-    auto decode(const std::vector<int> &tokens) const -> std::string {
-        return _decode_native(tokens);
-    }
-
-    auto _decode_native(const std::vector<int> &tokens) const -> std::string {
-        std::string ret;
-        ret.reserve(tokens.size() * 2);
-        for (auto token : tokens) {
-            std::string token_bytes;
-            auto iter = decoder_.find(token);
-            if (iter != decoder_.end()) {
-                token_bytes = iter->second;
-            } else {
-                iter = special_tokens_decoder.find(token);
-                if (iter != special_tokens_decoder.end()) {
-                    token_bytes = iter->second;
-                } else {
-                    throw std::runtime_error("unknown token: " + std::to_string(token));
-                }
-            }
-            ret += token_bytes;
-        }
-        return ret;
-    }
-
-    void init(std::unordered_map<std::string, int> special_encoder = {}) {
-        // init special encoder
-        std::string special_pattern;
-        for (const auto &item : special_encoder) {
-            if (!special_pattern.empty()) {
-                special_pattern += "|";
-            }
-            special_pattern += re2::RE2::QuoteMeta(item.first);
-        }
-        if (special_pattern.empty()) {
-            special_regex_ = nullptr;
-        } else {
-            special_regex_ = std::make_unique<re2::RE2>("(" + special_pattern + ")");
-        }
-
-        // encoder to enxoder_
-        for (auto item : vocab_map_) encoder_.emplace(item.first, item.second);
-        for (const auto &[k, v] : encoder_) {
-            decoder_.emplace(v, k);
-        }
-        assert(encoder_.size() == decoder_.size() && "Encoder and decoder must be of equal length; maybe you had duplicate token indices in your encoder?");
-
-        for (const auto &[k, v] : special_tokens_encoder) {
-            special_tokens_decoder.emplace(v, k);
-        }
-    }
-
-    void tokenize(const std::string &text, std::vector<token_id_t> &tokens, bool bos) override {
-        auto _ = encode(text);
-        for (auto it : _) tokens.emplace_back(it);
-        // no need to add bos
-    }
-
-    std::string detokenize(const std::vector<token_id_t> &tokens) override {
-        std::vector<int> _;
-        for (auto it : tokens) _.emplace_back(it);
-        return decode(_);
-    }
-
-private:
-    std::unique_ptr<re2::RE2> regex_;
-    std::unique_ptr<re2::RE2> special_regex_;
-    std::unordered_map<std::string, int> encoder_;
-    std::unordered_map<std::string, int> special_tokens_encoder;
-    std::unordered_map<int, std::string> decoder_;
-    std::unordered_map<int, std::string> special_tokens_decoder;
-};
-} // namespace mllm
 
 static const std::string PAT_STR = R"((?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]+|\s+(?:$|[^\S])|\s+)";
 
 class QWenTokenizer final {
 public:
-    explicit QWenTokenizer(const std::string &vocab_file) {
+    explicit QWenTokenizer(const std::string &vocab_file, const std::string &merge_file) {
         Module::initBackend(MLLM_CPU);
-        tokenizer = new TikTokenizer(vocab_file, PAT_STR);
+        tokenizer = new BPETokenizer(vocab_file);
 
-        // TODO init special encoder;
-        std::pair<std::string, int> bos_token;
-        std::pair<std::string, int> eos_token;
-        std::unordered_map<std::string, int> special_encoder_tmp = {
-            // TODO
-        };
-        tokenizer->init(special_encoder_tmp);
-        tokenizer->setSpecialToken(/*bos*/ bos_token.first, /*eos*/ eos_token.first);
+        regex_ = std::make_unique<re2::RE2>("(" + PAT_STR + ")");
+
+        // init byte encoder
+        std::vector<int> bs;
+        for (int i = 33 /*!*/; i < 127 /*~*/; ++i) bs.emplace_back(i);
+        for (int i = 161 /*¡*/; i < 173 /*¬*/; ++i) bs.emplace_back(i);
+        for (int i = 174 /*®*/; i < 256 /*ÿ*/; ++i) bs.emplace_back(i);
+        std::vector<int> cs = bs; // this is deep copy
+        int n = 0;
+        for (int b = 0; b < 256; ++b) {
+            if (std::find(bs.begin(), bs.end(), b) == bs.end()) {
+                bs.push_back(b);
+                cs.push_back(256 + n);
+                n++;
+            }
+        }
+        assert(bs.size() == cs.size() && "In init byte encoder, the bs and cs size should be same.");
+        for (auto i = 0U; i < bs.size(); ++i) {
+            byte_encoder_[bs[i]] = CHR(cs[i]);
+            byte_decoder_[CHR(cs[i])] = bs[i];
+        }
+
+        // init bpe ranks
+        auto merge_file_stream = std::ifstream(merge_file);
+        if (!merge_file_stream.good()) {
+            std::cout << "merge file is broken\n";
+            exit(0);
+        }
+        std::string line;
+        unsigned rank = 0;
+        while (std::getline(merge_file_stream, line)) {
+            if (line.empty()) {
+                continue;
+            }
+            if (line[0] == '#') {
+                continue;
+            }
+            bpe_ranks_[line] = rank;
+            rank++;
+        }
+        tokenizer->setMergeRank(bpe_ranks_);
     }
 
     ~QWenTokenizer() {
@@ -312,16 +107,47 @@ public:
 
     Tensor tokenize(std::string &text, int str_i = 0) {
         std::vector<token_id_t> ret;
-        tokenizer->tokenize(text, ret, /*bos*/ false);
+        re2::StringPiece input(text);
+        std::string piece;
+        while (re2::RE2::FindAndConsume(&input, *regex_, &piece)) {
+            // look up table
+            std::string token;
+            for (auto b : UTF8(piece)) token += byte_encoder_[b];
+
+            // using bpe
+            std::vector<token_id_t> tmp;
+            tokenizer->tokenize(token, tmp, /*bos*/ false, /*byte fallback*/ true, "");
+
+            // FIXME: why the tokenizer will generate a 2.0000 at the end of all tokens?
+            ret.insert(ret.end(), tmp.begin(), tmp.end() - 1);
+        }
+        // FIXME if we need bos or not?
+        ret.insert(ret.begin(), 151643);
         return Tokenizer::tokens2Input(ret);
     }
 
+    // FIXME std::string += std::string has performance issues when string is large.
+    std::string _byte_decode_(const std::string &text) {
+        std::string ret;
+        auto _ = ORD(text);
+        for (auto i : _) ret += byte_decoder_[CHR(i)];
+        return ret;
+    }
+
     std::string detokenize(const std::vector<token_id_t> &tokens) {
-        return tokenizer->detokenize(tokens);
+        return _byte_decode_(tokenizer->detokenize(tokens));
     }
 
     std::pair<std::string, unsigned> detokenize(Tensor &result) {
-        // TODO argmax can't be used here due to utf-8.
+        assert(result.batch() == 1);
+        assert(result.head() == 1);
+        vector<float> scores;
+        for (int i = 0; i < result.dimension(); ++i) {
+            auto value = result.dataAt<float>(0, 0, result.sequence() - 1, i);
+            scores.push_back(value);
+        }
+        auto token_idx = this->argmax(scores);
+        return {_byte_decode_(tokenizer->detokenize({token_idx})), token_idx};
     }
 
 private:
@@ -332,12 +158,18 @@ private:
         return std::max_element(scores.begin(), scores.end()) - scores.begin();
     }
 
-    TikTokenizer *tokenizer;
-    std::unordered_map<char, std::string> byte_encoder;
-
 public:
-    token_id_t eos_id = 151643,
-               bos_id = 151643, im_start_id = 151644, im_end_id = 151645;
+    BPETokenizer *tokenizer;
+    std::unique_ptr<re2::RE2> regex_;
+    std::unordered_map<int, std::string> byte_encoder_;
+    std::unordered_map<std::string, int> byte_decoder_;
+    std::unordered_map<std::string, unsigned int> bpe_ranks_;
+    token_id_t eos_id_ = 151643,
+               bos_id_ = 151643, im_start_id_ = 151644, im_end_id_ = 151645;
 };
+
+#undef UTF8
+#undef CHR
+#undef ORD
 
 #endif //! TOKENIZATION_QWEN_HPP
