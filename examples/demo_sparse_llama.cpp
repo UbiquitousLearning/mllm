35aaff8fdb072f2955eba235fd29a07989d6afc9
yirongjie <yirj0809@gmail.com>
2024-07-15 00:18:05 +0000

    fix: demo sparse_relullama remove ReLULlama_predictor.

--- examples/demo_sparse_llama.cpp
+++ examples/demo_sparse_llama.cpp
@@ -13,8 +13,8 @@ using namespace mllm;
 int main(int argc, char **argv) {
     cmdline::parser cmdParser;
     cmdParser.add<string>("vocab", 'v', "specify mllm tokenizer model path", false, "../vocab/relu_llama_vocab.mllm");
-    cmdParser.add<string>("model", 'm', "specify mllm model path", false, "../ReLULlama_sparse_q4_k.mllm");
-    cmdParser.add<string>("predictor", 'p', "specify mllm model predictor path", false, "../ReLULlama_predictor.mllm");
+    cmdParser.add<string>("model", 'm', "specify mllm model path", false, "../models/ReLULlama_sparse_q4_k.mllm");
+    // cmdParser.add<string>("predictor", 'p', "specify mllm model predictor path", false, "../models/ReLULlama_predictor.mllm");
     cmdParser.add<int>("limits", 'l',  "max KV cache size", false, 600);
     cmdParser.add<int>("thread", 't', "num of threads", false, 4);
     cmdParser.parse_check(argc, argv);
@@ -23,14 +23,15 @@ int main(int argc, char **argv) {
     string model_path = cmdParser.get<string>("model");
     int tokens_limit = cmdParser.get<int>("limits");
     CPUBackend::cpu_threads = cmdParser.get<int>("thread");
-    string predictor_path = cmdParser.get<string>("predictor");
+    // string predictor_path = cmdParser.get<string>("predictor");
 
     auto tokenizer = LLaMATokenizer(vocab_path);
 
     LLaMAConfig config(tokens_limit, "7B", HFHUBROPE);
     auto is_down_sparse = true;
     auto model = SparseLLaMAModel(config, is_down_sparse);
-    MultiFileParamLoader param_loader({model_path, predictor_path, "../ReLULlama_q4_k.mllm"});
+    // MultiFileParamLoader param_loader({model_path, predictor_path, "../ReLULlama_q4_k.mllm"});
+    MultiFileParamLoader param_loader({model_path, "../models/ReLULlama_q4_k.mllm"});
     model.load(param_loader);
 
     vector<string> in_strs = {
@@ -41,8 +42,6 @@ int main(int argc, char **argv) {
     for (int i = 0; i < in_strs.size(); ++i) {
         auto in_str = in_strs[i];
         auto input_tensor = tokenizer.tokenize(in_str, i);
-//        std::cout << "Input tensor: " << std::endl;
-//        input_tensor.printData<float>();
         std::cout << "[Q] " << in_str << std::endl;
         std::cout << "[A] " << std::flush;
         for (int step = 0; step < 100; step++) {
