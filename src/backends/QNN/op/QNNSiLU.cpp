d8628df640b25ebf020241fb16ed100d0296d898
xudaliang <xudaliang@pku.edu.cn>
2024-07-23 19:33:04 +0800

    feat : support FFN in FP16.

--- src/backends/QNN/op/QNNSiLU.cpp
+++ src/backends/QNN/op/QNNSiLU.cpp
@@ -31,7 +31,7 @@ ErrorCode QNNSiLU::setUp(vector<shared_ptr<Tensor>> inputs, vector<shared_ptr<Te
                                                .name = outName.c_str(),
                                                .type = getOutputTensorType(outputs[0]),
                                                .dataFormat = QNN_TENSOR_DATA_FORMAT_FLAT_BUFFER,
-                                               .dataType = QNN_DATATYPE_FLOAT_32,
+                                               .dataType = QNN_DATATYPE_FLOAT_16,
                                                .quantizeParams = {QNN_DEFINITION_UNDEFINED,
                                                     QNN_QUANTIZATION_ENCODING_UNDEFINED,
                                                     {.scaleOffsetEncoding = {.scale  = 0.0000000000000000f,
