96669774d7224fe10d441c0eb2b1c95bf504bde8
xudaliang <xudaliang@pku.edu.cn>
2024-08-05 22:42:29 +0800

    fix :  Prefill only model bugs.

--- demo/qnn/qnn_opt_smoothquant.cpp
+++ demo/qnn/qnn_opt_smoothquant.cpp
@@ -162,8 +162,8 @@ int main(int argc, char **argv) {
     npuExe.setup(&npuNet);
     Executor interExe(&inter_param_loader);
     interExe.setup(&interNet);
-    Executor cpuExe(&cpu_decoding_param_loader);
-    cpuExe.setup(&cpuNet);
+    // Executor cpuExe(&cpu_decoding_param_loader);
+    // cpuExe.setup(&cpuNet);
 
     vector<string> in_strs = {
         "<|im_start|>system\nYou are a helpful assistant.<| im_end |>\n<| im_start |>user\nGive me a short introduction to large language model.<| im_end |>\n<| im_start |> assistant\n\n",
@@ -287,7 +287,7 @@ int main(int argc, char **argv) {
         }
     }
 
-    cpuExe.perf();
+    // cpuExe.perf();
 
     // free memory
     // for (auto *op : npu_ctx->net_ops) {
