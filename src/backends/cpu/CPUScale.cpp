8dc3928465466a07015a8d10efb3bf70c6f6f61f
yirongjie <yirj0809@gmail.com>
2024-08-26 12:46:55 +0000

    fix: +-*/ for old front end

--- src/backends/cpu/CPUScale.cpp
+++ src/backends/cpu/CPUScale.cpp
@@ -1,5 +1,6 @@
 
 #include "CPUScale.hpp"
+#include "compute/Arithmetic.hpp"
 
 namespace mllm {
 
@@ -23,9 +24,21 @@ ErrorCode CPUScale::reshape(vector<shared_ptr<Tensor>> inputs, vector<shared_ptr
 }
 
 ErrorCode CPUScale::execute(vector<shared_ptr<Tensor>> inputs, vector<shared_ptr<Tensor>> outputs) {
-
     auto & input = inputs[0];
     auto & output = outputs[0];
+    if(bias_ == 0.0F){
+#pragma omp parallel for collapse(3) num_threads(CPUBackend::cpu_threads)
+        for (int n = 0; n < input->batch(); ++n) {
+            for (int c = 0; c < input->head(); ++c) {
+                for (int h = 0; h < input->sequence(); ++h) {
+                    mllm_mul_fp32(input->ptrAt<float>(n, c, h, 0),scale_, 
+                        outputs[0]->ptrAt<float>(n, c, h, 0), input->dimension());
+                }
+            }
+        }
+        return Op::execute(inputs, outputs);
+    }
+
     if(inputs[0]->masterTensor() == nullptr && outputs[0]->masterTensor() == nullptr && inputs[0]->ctype() == outputs[0]->ctype()) {
         auto copy_size = input->batch() * input->head() * input->sequence() * input->dimension();
         auto in_ptr = inputs[0]->hostPtr<float>();
