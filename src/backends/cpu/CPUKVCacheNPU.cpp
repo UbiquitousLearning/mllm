dfac9d5c4b06a88a04d49468838fe05d138829e7
oreomaker <zh002919@outlook.com>
2024-11-18 22:34:49 +0800

    feat: KVCacheNPU GQA

--- src/backends/cpu/CPUKVCacheNPU.cpp
+++ src/backends/cpu/CPUKVCacheNPU.cpp
@@ -14,13 +14,14 @@ CPUKVCacheNPU::CPUKVCacheNPU(Backend *bn, string opName, int n_rep, int cache_ma
     // TODO: Chaning it to FP16
     cache_.setDtype(MLLM_TYPE_F16);
     cache_limit_ = cache_max;
+    n_rep_ = n_rep;
 }
 
 ErrorCode CPUKVCacheNPU::reshape(vector<shared_ptr<Tensor>> inputs, vector<shared_ptr<Tensor>> outputs) {
     assert(inputs.size() == 1);
     assert(outputs.size() == 1);
     if (cache_seq_len_ < 0) {
-        cache_.reshape(inputs[0]->batch(), inputs[0]->head(), cache_limit_, inputs[0]->dimension());
+        cache_.reshape(inputs[0]->batch(), inputs[0]->head() * n_rep_, cache_limit_, inputs[0]->dimension());
         cache_.setName(name() + ".Cache");
         cache_.alloc();
         cache_seq_len_ = 0;
@@ -45,7 +46,7 @@ ErrorCode CPUKVCacheNPU::reshape(vector<shared_ptr<Tensor>> inputs, vector<share
     }
 #endif
 
-    outputs[0]->reshape(inputs[0]->batch(), inputs[0]->head(), inputs[0]->sequence() + cache_seq_len_, inputs[0]->dimension());
+    outputs[0]->reshape(inputs[0]->batch(), inputs[0]->head() * n_rep_, inputs[0]->sequence() + cache_seq_len_, inputs[0]->dimension());
 
     if (inputs[0]->sequence() + cache_seq_len_ > cache_limit_) {
         MLLM_LOG_ERROR_STREAM << "\n[ERROR]: Current tokens exceed cache limit: " << inputs[0]->sequence() + cache_seq_len_ << ">" << cache_limit_ << ";" << "\n         Please set args `--limits` >" << cache_limit_ << std::endl;
@@ -61,9 +62,118 @@ ErrorCode CPUKVCacheNPU::load(AbstructLoader &loader) {
 }
 
 ErrorCode CPUKVCacheNPU::execute(vector<shared_ptr<Tensor>> inputs, vector<shared_ptr<Tensor>> outputs) {
+    // Group Query Attention
+    if (n_rep_ > 1 && isDecoding) {
+        int cache_seq_len_old = cache_seq_len_;
+        cache_seq_len_ += inputs[0]->sequence();
+        if (cache_.ctype() == BSHD) {
+            for (int b = 0; b < cache_.batch(); ++b) {
+                for (int h = inputs[0]->head() - 1; h >= 0; --h) {
+#pragma omp parallel for collapse(2) num_threads(thread_count)
+                    for (int seq = cache_seq_len_old; seq < cache_seq_len_; ++seq) {
+                        for (int i_rep = 0; i_rep < n_rep_; ++i_rep) {
+                            auto cache_head = h * n_rep_ + i_rep;
+                            if (cache_.dtype() == MLLM_TYPE_F32) {
+                                auto src_ptr =
+                                    inputs[0]->ptrAt<float>(b, h, seq - cache_seq_len_old, 0);
+                                auto dest_ptr = cache_.ptrAt<float>(b, cache_head, seq, 0);
+                                int copy_size = cache_.dimension();
+                                memcpy(dest_ptr, src_ptr, copy_size * sizeof(float));
+                            } else if (cache_.dtype() == MLLM_TYPE_F16) {
+                                auto src_ptr =
+                                    inputs[0]->ptrAt<mllm_fp16_t>(b, h, seq - cache_seq_len_old, 0);
+                                auto dest_ptr = cache_.ptrAt<mllm_fp16_t>(b, cache_head, seq, 0);
+                                int copy_size = cache_.dimension();
+                                memcpy(dest_ptr, src_ptr, copy_size * sizeof(mllm_fp16_t));
+                            }
+                        }
+                    }
+                }
+            }
+        } else if (cache_.ctype() == BHDS) {
+            for (int b = 0; b < cache_.batch(); ++b) {
+                for (int h = inputs[0]->head() - 1; h >= 0; --h) {
+#pragma omp parallel for collapse(2) num_threads(thread_count)
+                    for (int d = 0; d < inputs[0]->dimension(); ++d) {
+                        for (int i_rep = 0; i_rep < n_rep_; ++i_rep) {
+                            auto cache_head = h * n_rep_ + i_rep;
+                            if (cache_.dtype() == MLLM_TYPE_F32) {
+                                auto src_ptr = inputs[0]->ptrAt<float>(b, h, 0, d);
+                                auto dest_ptr =
+                                    cache_.ptrAt<float>(b, cache_head, cache_seq_len_old, d);
+                                int copy_size = cache_seq_len_ - cache_seq_len_old;
+                                memcpy(dest_ptr, src_ptr, copy_size * sizeof(float));
+                            } else if (cache_.dtype() == MLLM_TYPE_F16) {
+                                auto src_ptr = inputs[0]->ptrAt<mllm_fp16_t>(b, h, 0, d);
+                                auto dest_ptr =
+                                    cache_.ptrAt<mllm_fp16_t>(b, cache_head, cache_seq_len_old, d);
+                                int copy_size = cache_seq_len_ - cache_seq_len_old;
+                                memcpy(dest_ptr, src_ptr, copy_size * sizeof(mllm_fp16_t));
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            std::cout << "ERROR Ctype in KVCcache;" << std::endl;
+        }
+        return MLLM_NO_ERROR;
+    }
+    if (n_rep_ > 1 && !isDecoding) {
+        if (cache_.ctype() == BSHD) {
+            for (int b = 0; b < cache_.batch(); ++b) {
+                for (int h = 0; h < inputs[0]->head(); ++h) {
+#pragma omp parallel for collapse(2) num_threads(thread_count)
+                    for (int seq = 0; seq < inputs[0]->sequence(); ++seq) {
+                        for (int i_rep = 0; i_rep < n_rep_; ++i_rep) {
+                            auto cache_head = h * n_rep_ + i_rep;
+                            if (cache_.dtype() == MLLM_TYPE_F32) {
+                                auto src_ptr =
+                                    inputs[0]->ptrAt<float>(b, h, seq, 0);
+                                auto dest_ptr = cache_.ptrAt<float>(b, cache_head, cache_seq_len_ + seq, 0);
+                                memcpy(dest_ptr, src_ptr, inputs[0]->dimension() * sizeof(float));
+                            } else if (cache_.dtype() == MLLM_TYPE_F16) {
+                                auto src_ptr =
+                                    inputs[0]->ptrAt<mllm_fp16_t>(b, h, seq, 0);
+                                auto dest_ptr = cache_.ptrAt<mllm_fp16_t>(b, cache_head, cache_seq_len_ + seq, 0);
+                                memcpy(dest_ptr, src_ptr, inputs[0]->dimension() * sizeof(mllm_fp16_t));
+                            }
+                        }
+                    }
+                }
+            }
+        } else if (cache_.ctype() == BHDS) {
+            for (int b = 0; b < cache_.batch(); ++b) {
+                for (int h = 0; h < inputs[0]->head(); ++h) {
+#pragma omp parallel for collapse(2) num_threads(thread_count)
+                    for (int d = 0; d < inputs[0]->dimension(); ++d) {
+                        for (int i_rep = 0; i_rep < n_rep_; ++i_rep) {
+                            auto cache_head = h * n_rep_ + i_rep;
+                            if (cache_.dtype() == MLLM_TYPE_F32) {
+                                auto src_ptr = inputs[0]->ptrAt<float>(b, h, 0, d);
+                                auto dest_ptr =
+                                    cache_.ptrAt<float>(b, cache_head, cache_seq_len_, d);
+                                memcpy(dest_ptr, src_ptr, inputs[0]->sequence() * sizeof(float));
+                            } else if (cache_.dtype() == MLLM_TYPE_F16) {
+                                auto src_ptr = inputs[0]->ptrAt<mllm_fp16_t>(b, h, 0, d);
+                                auto dest_ptr =
+                                    cache_.ptrAt<mllm_fp16_t>(b, cache_head, cache_seq_len_, d);
+                                memcpy(dest_ptr, src_ptr, inputs[0]->sequence() * sizeof(mllm_fp16_t));
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            std::cout << "ERROR Ctype in KVCcache;" << std::endl;
+        }
+        cache_seq_len_ += inputs[0]->sequence();
+        return MLLM_NO_ERROR;
+    }
 
+    // q head == kv cache head
     // when decoding, the input will deepCopy from cache, no need to execute
-    if (isDecoding) {
+    if (n_rep_ <= 1 && isDecoding) {
         cache_seq_len_ += inputs[0]->sequence();
         return MLLM_NO_ERROR;
     }
@@ -111,22 +221,7 @@ ErrorCode CPUKVCacheNPU::execute(vector<shared_ptr<Tensor>> inputs, vector<share
             }
         }
     } else { // naive case
-#pragma omp parallel for collapse(4) num_threads(thread_count)
-        for (int b = 0; b < cache_.batch(); ++b) {
-            for (int h = 0; h < inputs[0]->head(); ++h) {
-                for (int s = 0; s < inputs[0]->sequence(); ++s) {
-                    for (int d = 0; d < inputs[0]->dimension(); ++d) {
-                        if (cache_.dtype() == MLLM_TYPE_F32) {
-                            cache_.setDataAt<float>(b, h, cache_seq_len_ + s, d, inputs[0]->dataAt<float>(b, h, s, d));
-                        } else if (cache_.dtype() == MLLM_TYPE_F16) {
-                            auto src_ptr = inputs[0]->ptrAt<mllm_fp16_t>(b, h, s, d);
-                            auto dest_ptr = cache_.ptrAt<mllm_fp16_t>(b, h, cache_seq_len_ + s, d);
-                            memcpy(dest_ptr, src_ptr, 1 * sizeof(mllm_fp16_t));
-                        }
-                    }
-                }
-            }
-        }
+        std::cout << "ERROR Ctype in KVCcache;" << std::endl;
     }
 
     cache_seq_len_ += inputs[0]->sequence();
