2b204723b88a4e09756ea3879130c2b5ea27ce93
oreomaker <zh002919@outlook.com>
2024-11-21 16:00:48 +0800

    refactor: simplify kvcache npu

--- src/backends/cpu/CPUKVCacheNPU.cpp
+++ src/backends/cpu/CPUKVCacheNPU.cpp
@@ -62,169 +62,115 @@ ErrorCode CPUKVCacheNPU::load(AbstructLoader &loader) {
 }
 
 ErrorCode CPUKVCacheNPU::execute(vector<shared_ptr<Tensor>> inputs, vector<shared_ptr<Tensor>> outputs) {
-    // Group Query Attention
-    if (n_rep_ > 1 && isDecoding) {
-        int cache_seq_len_old = cache_seq_len_;
-        cache_seq_len_ += inputs[0]->sequence();
-        if (cache_.ctype() == BSHD) {
-            for (int b = 0; b < cache_.batch(); ++b) {
-                for (int h = inputs[0]->head() - 1; h >= 0; --h) {
+    int input_seq = inputs[0]->sequence();
+    int cache_seq_len_old = cache_seq_len_;
+    cache_seq_len_ += inputs[0]->sequence();
+
+    if (isDecoding) {
+        // Group Query Attention
+        if (n_rep_ > 1) {
+            if (cache_.ctype() == BSHD) {
+                for (int b = 0; b < cache_.batch(); ++b) {
+                    for (int h = inputs[0]->head() - 1; h >= 0; --h) {
 #pragma omp parallel for collapse(2) num_threads(thread_count)
-                    for (int seq = cache_seq_len_old; seq < cache_seq_len_; ++seq) {
-                        for (int i_rep = 0; i_rep < n_rep_; ++i_rep) {
-                            auto cache_head = h * n_rep_ + i_rep;
-                            if (cache_.dtype() == MLLM_TYPE_F32) {
-                                auto src_ptr =
-                                    inputs[0]->ptrAt<float>(b, h, seq - cache_seq_len_old, 0);
-                                auto dest_ptr = cache_.ptrAt<float>(b, cache_head, seq, 0);
-                                int copy_size = cache_.dimension();
-                                memcpy(dest_ptr, src_ptr, copy_size * sizeof(float));
-                            } else if (cache_.dtype() == MLLM_TYPE_F16) {
-                                auto src_ptr =
-                                    inputs[0]->ptrAt<mllm_fp16_t>(b, h, seq - cache_seq_len_old, 0);
-                                auto dest_ptr = cache_.ptrAt<mllm_fp16_t>(b, cache_head, seq, 0);
-                                int copy_size = cache_.dimension();
-                                memcpy(dest_ptr, src_ptr, copy_size * sizeof(mllm_fp16_t));
+                        for (int seq = cache_seq_len_old; seq < cache_seq_len_; ++seq) {
+                            for (int i_rep = 0; i_rep < n_rep_; ++i_rep) {
+                                auto cache_head = h * n_rep_ + i_rep;
+                                if (cache_.dtype() == MLLM_TYPE_F32) {
+                                    auto src_ptr =
+                                        inputs[0]->ptrAt<float>(b, h, input_seq, 0);
+                                    auto dest_ptr = cache_.ptrAt<float>(b, cache_head, seq, 0);
+                                    int copy_size = cache_.dimension();
+                                    memcpy(dest_ptr, src_ptr, copy_size * sizeof(float));
+                                } else if (cache_.dtype() == MLLM_TYPE_F16) {
+                                    auto src_ptr =
+                                        inputs[0]->ptrAt<mllm_fp16_t>(b, h, input_seq, 0);
+                                    auto dest_ptr = cache_.ptrAt<mllm_fp16_t>(b, cache_head, seq, 0);
+                                    int copy_size = cache_.dimension();
+                                    memcpy(dest_ptr, src_ptr, copy_size * sizeof(mllm_fp16_t));
+                                }
                             }
                         }
                     }
                 }
-            }
-        } else if (cache_.ctype() == BHDS) {
-            for (int b = 0; b < cache_.batch(); ++b) {
-                for (int h = inputs[0]->head() - 1; h >= 0; --h) {
+            } else if (cache_.ctype() == BHDS) {
+                for (int b = 0; b < cache_.batch(); ++b) {
+                    for (int h = inputs[0]->head() - 1; h >= 0; --h) {
 #pragma omp parallel for collapse(2) num_threads(thread_count)
-                    for (int d = 0; d < inputs[0]->dimension(); ++d) {
-                        for (int i_rep = 0; i_rep < n_rep_; ++i_rep) {
-                            auto cache_head = h * n_rep_ + i_rep;
-                            if (cache_.dtype() == MLLM_TYPE_F32) {
-                                auto src_ptr = inputs[0]->ptrAt<float>(b, h, 0, d);
-                                auto dest_ptr =
-                                    cache_.ptrAt<float>(b, cache_head, cache_seq_len_old, d);
-                                int copy_size = cache_seq_len_ - cache_seq_len_old;
-                                memcpy(dest_ptr, src_ptr, copy_size * sizeof(float));
-                            } else if (cache_.dtype() == MLLM_TYPE_F16) {
-                                auto src_ptr = inputs[0]->ptrAt<mllm_fp16_t>(b, h, 0, d);
-                                auto dest_ptr =
-                                    cache_.ptrAt<mllm_fp16_t>(b, cache_head, cache_seq_len_old, d);
-                                int copy_size = cache_seq_len_ - cache_seq_len_old;
-                                memcpy(dest_ptr, src_ptr, copy_size * sizeof(mllm_fp16_t));
+                        for (int d = 0; d < inputs[0]->dimension(); ++d) {
+                            for (int i_rep = 0; i_rep < n_rep_; ++i_rep) {
+                                auto cache_head = h * n_rep_ + i_rep;
+                                if (cache_.dtype() == MLLM_TYPE_F32) {
+                                    auto src_ptr = inputs[0]->ptrAt<float>(b, h, 0, d);
+                                    auto dest_ptr =
+                                        cache_.ptrAt<float>(b, cache_head, cache_seq_len_old, d);
+                                    memcpy(dest_ptr, src_ptr, input_seq * sizeof(float));
+                                } else if (cache_.dtype() == MLLM_TYPE_F16) {
+                                    auto src_ptr = inputs[0]->ptrAt<mllm_fp16_t>(b, h, 0, d);
+                                    auto dest_ptr =
+                                        cache_.ptrAt<mllm_fp16_t>(b, cache_head, cache_seq_len_old, d);
+                                    memcpy(dest_ptr, src_ptr, input_seq * sizeof(mllm_fp16_t));
+                                }
                             }
                         }
                     }
                 }
+            } else {
+                std::cout << "ERROR Ctype in KVCcache;" << std::endl;
             }
-        } else {
-            std::cout << "ERROR Ctype in KVCcache;" << std::endl;
         }
-        return MLLM_NO_ERROR;
-    }
-    if (n_rep_ > 1 && !isDecoding) {
-        if (cache_.ctype() == BSHD) {
-            for (int b = 0; b < cache_.batch(); ++b) {
-                for (int h = 0; h < inputs[0]->head(); ++h) {
-#pragma omp parallel for collapse(2) num_threads(thread_count)
-                    for (int seq = 0; seq < inputs[0]->sequence(); ++seq) {
-                        for (int i_rep = 0; i_rep < n_rep_; ++i_rep) {
-                            auto cache_head = h * n_rep_ + i_rep;
-                            if (cache_.dtype() == MLLM_TYPE_F32) {
-                                auto src_ptr =
-                                    inputs[0]->ptrAt<float>(b, h, seq, 0);
-                                auto dest_ptr = cache_.ptrAt<float>(b, cache_head, cache_seq_len_ + seq, 0);
-                                memcpy(dest_ptr, src_ptr, inputs[0]->dimension() * sizeof(float));
-                            } else if (cache_.dtype() == MLLM_TYPE_F16) {
-                                auto src_ptr =
-                                    inputs[0]->ptrAt<mllm_fp16_t>(b, h, seq, 0);
-                                auto dest_ptr = cache_.ptrAt<mllm_fp16_t>(b, cache_head, cache_seq_len_ + seq, 0);
-                                memcpy(dest_ptr, src_ptr, inputs[0]->dimension() * sizeof(mllm_fp16_t));
-                            }
-                        }
-                    }
-                }
-            }
-        } else if (cache_.ctype() == BHDS) {
-            for (int b = 0; b < cache_.batch(); ++b) {
-                for (int h = 0; h < inputs[0]->head(); ++h) {
-#pragma omp parallel for collapse(2) num_threads(thread_count)
-                    for (int d = 0; d < inputs[0]->dimension(); ++d) {
-                        for (int i_rep = 0; i_rep < n_rep_; ++i_rep) {
-                            auto cache_head = h * n_rep_ + i_rep;
-                            if (cache_.dtype() == MLLM_TYPE_F32) {
-                                auto src_ptr = inputs[0]->ptrAt<float>(b, h, 0, d);
-                                auto dest_ptr =
-                                    cache_.ptrAt<float>(b, cache_head, cache_seq_len_, d);
-                                memcpy(dest_ptr, src_ptr, inputs[0]->sequence() * sizeof(float));
-                            } else if (cache_.dtype() == MLLM_TYPE_F16) {
-                                auto src_ptr = inputs[0]->ptrAt<mllm_fp16_t>(b, h, 0, d);
-                                auto dest_ptr =
-                                    cache_.ptrAt<mllm_fp16_t>(b, cache_head, cache_seq_len_, d);
-                                memcpy(dest_ptr, src_ptr, inputs[0]->sequence() * sizeof(mllm_fp16_t));
-                            }
-                        }
-                    }
-                }
-            }
-        } else {
-            std::cout << "ERROR Ctype in KVCcache;" << std::endl;
-        }
-        cache_seq_len_ += inputs[0]->sequence();
+        // normal attention decoding doesn't need to copy the input to the cache
         return MLLM_NO_ERROR;
     }
 
-    // q head == kv cache head
-    // when decoding, the input will deepCopy from cache, no need to execute
-    if (n_rep_ <= 1 && isDecoding) {
-        cache_seq_len_ += inputs[0]->sequence();
-        return MLLM_NO_ERROR;
-    }
-
-    if (cache_.ctype() == BSHD && inputs[0]->ctype() == BSHD) { // 'K'
-#pragma omp parallel for collapse(3) num_threads(thread_count)
+    // prefilling execution, copy the input to the cache
+    if (cache_.ctype() == BSHD) {
         for (int b = 0; b < cache_.batch(); ++b) {
             for (int h = 0; h < inputs[0]->head(); ++h) {
+#pragma omp parallel for collapse(2) num_threads(thread_count)
                 for (int seq = 0; seq < inputs[0]->sequence(); ++seq) {
-                    if (cache_.dtype() == MLLM_TYPE_F32) {
-                        auto src_ptr = inputs[0]->ptrAt<float>(b, h, seq, 0);
-                        auto dest_ptr = cache_.ptrAt<float>(b, h, cache_seq_len_ + seq, 0);
-                        memcpy(dest_ptr, src_ptr, cache_.dimension() * sizeof(float));
-                    } else if (cache_.dtype() == MLLM_TYPE_F16) {
-                        auto src_ptr = inputs[0]->ptrAt<mllm_fp16_t>(b, h, seq, 0);
-                        auto dest_ptr = cache_.ptrAt<mllm_fp16_t>(b, h, cache_seq_len_ + seq, 0);
-                        memcpy(dest_ptr, src_ptr, cache_.dimension() * sizeof(mllm_fp16_t));
-                    } else if (cache_.dtype() == MLLM_TYPE_I8) {
-                        auto src_ptr = inputs[0]->ptrAt<int8_t>(b, h, seq, 0);
-                        auto dest_ptr = cache_.ptrAt<int8_t>(b, h, cache_seq_len_ + seq, 0);
-                        memcpy(dest_ptr, src_ptr, cache_.dimension() * sizeof(int8_t));
+                    for (int i_rep = 0; i_rep < n_rep_; ++i_rep) {
+                        auto cache_head = h * n_rep_ + i_rep;
+                        if (cache_.dtype() == MLLM_TYPE_F32) {
+                            auto src_ptr =
+                                inputs[0]->ptrAt<float>(b, h, seq, 0);
+                            auto dest_ptr = cache_.ptrAt<float>(b, cache_head, cache_seq_len_old + seq, 0);
+                            memcpy(dest_ptr, src_ptr, inputs[0]->dimension() * sizeof(float));
+                        } else if (cache_.dtype() == MLLM_TYPE_F16) {
+                            auto src_ptr =
+                                inputs[0]->ptrAt<mllm_fp16_t>(b, h, seq, 0);
+                            auto dest_ptr = cache_.ptrAt<mllm_fp16_t>(b, cache_head, cache_seq_len_old + seq, 0);
+                            memcpy(dest_ptr, src_ptr, inputs[0]->dimension() * sizeof(mllm_fp16_t));
+                        }
                     }
                 }
             }
         }
-    } else if (cache_.ctype() == BHDS && inputs[0]->ctype() == BHDS) { // 'V'
-#pragma omp parallel for collapse(3) num_threads(thread_count)
+    } else if (cache_.ctype() == BHDS) {
         for (int b = 0; b < cache_.batch(); ++b) {
             for (int h = 0; h < inputs[0]->head(); ++h) {
+#pragma omp parallel for collapse(2) num_threads(thread_count)
                 for (int d = 0; d < inputs[0]->dimension(); ++d) {
-                    if (cache_.dtype() == MLLM_TYPE_F32) {
-                        auto src_ptr = inputs[0]->ptrAt<float>(b, h, 0, d);
-                        auto dest_ptr = cache_.ptrAt<float>(b, h, cache_seq_len_, d);
-                        memcpy(dest_ptr, src_ptr, inputs[0]->sequence() * sizeof(float));
-                    } else if (cache_.dtype() == MLLM_TYPE_F16) {
-                        auto src_ptr = inputs[0]->ptrAt<mllm_fp16_t>(b, h, 0, d);
-                        auto dest_ptr = cache_.ptrAt<mllm_fp16_t>(b, h, cache_seq_len_, d);
-                        memcpy(dest_ptr, src_ptr, inputs[0]->sequence() * sizeof(mllm_fp16_t));
-                    } else if (cache_.dtype() == MLLM_TYPE_I8) {
-                        auto src_ptr = inputs[0]->ptrAt<int8_t>(b, h, 0, d);
-                        auto dest_ptr = cache_.ptrAt<int8_t>(b, h, cache_seq_len_, d);
-                        memcpy(dest_ptr, src_ptr, inputs[0]->sequence() * sizeof(int8_t));
+                    for (int i_rep = 0; i_rep < n_rep_; ++i_rep) {
+                        auto cache_head = h * n_rep_ + i_rep;
+                        if (cache_.dtype() == MLLM_TYPE_F32) {
+                            auto src_ptr = inputs[0]->ptrAt<float>(b, h, 0, d);
+                            auto dest_ptr =
+                                cache_.ptrAt<float>(b, cache_head, cache_seq_len_old, d);
+                            memcpy(dest_ptr, src_ptr, inputs[0]->sequence() * sizeof(float));
+                        } else if (cache_.dtype() == MLLM_TYPE_F16) {
+                            auto src_ptr = inputs[0]->ptrAt<mllm_fp16_t>(b, h, 0, d);
+                            auto dest_ptr =
+                                cache_.ptrAt<mllm_fp16_t>(b, cache_head, cache_seq_len_old, d);
+                            memcpy(dest_ptr, src_ptr, inputs[0]->sequence() * sizeof(mllm_fp16_t));
+                        }
                     }
                 }
             }
         }
-    } else { // naive case
+    } else {
         std::cout << "ERROR Ctype in KVCcache;" << std::endl;
     }
-
-    cache_seq_len_ += inputs[0]->sequence();
 
     return Op::execute(inputs, outputs);
 }
