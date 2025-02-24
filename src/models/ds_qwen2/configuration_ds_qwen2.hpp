ca062e6c083f541906821ae5df3d0883f14c2ec3
chenghuaWang <2923277184@qq.com>
2025-02-24 10:52:04 +0000

    feat: deepseek distilled qwen2 1.5B

--- src/models/ds_qwen2/configuration_ds_qwen2.hpp
+++ src/models/ds_qwen2/configuration_ds_qwen2.hpp
@@ -1,16 +1,14 @@
 /**
- * @file configuration_gemma.hpp
- * @author Chenghua Wang (chenghua.wang.edu@gmail.com)
- * @brief configuration file of qwen llm.
+ * @file configuration_ds_qwen2.hpp
+ * @author chenghua Wang (chenghua.wang.edu@gmail.com)
  * @version 0.1
- * @date 2024-04-03
+ * @date 2025-02-24
  *
- * @copyright Copyright (c) 2024
+ * @copyright Copyright (c) 2025
  *
  */
+#pragma once
 
-#ifndef CONFIG_QWEN_HPP
-#define CONFIG_QWEN_HPP
 #include "Types.hpp"
 #include "models/transformer/configuration_transformer.hpp"
 #include <cctype>
@@ -45,24 +43,6 @@ public:
             lm_head_name = "lm_head";
             break;
         }
-        case RoPEType::LLAMAROPE: /*the qwen2 is same to llama*/ {
-            blk_name = "layers.";
-            _attn_base_name = "attention.";
-            _ffn_base_name = "feed_forward.";
-            _q_proj_name = "wq";
-            _k_proj_name = "wk";
-            _v_proj_name = "wv";
-            _o_proj_name = "wo";
-            _gate_proj_name = "w1";
-            _up_proj_name = "w3";
-            _down_proj_name = "w2";
-            _attn_norm_name = "attention_norm";
-            _ffn_norm_name = "ffn_norm";
-            token_embd_name = "tok_embeddings";
-            post_norm_name = "norm";
-            lm_head_name = "output";
-            break;
-        }
         default: {
             throw std::runtime_error("Unsupported gemma RoPE type");
         }
@@ -77,61 +57,13 @@ public:
 };
 
 struct QWenConfig : public TransformerConfig {
-    explicit QWenConfig(int token_limit, string billions = "0.5B", RoPEType type = RoPEType::HFHUBROPE) :
+    explicit QWenConfig(int token_limit, string billions = "1.5B", RoPEType type = RoPEType::HFHUBROPE) :
         cache_limit(token_limit) {
         names_config.init(type);
         string billionsType;
         std::transform(billions.begin(), billions.end(), std::back_inserter(billionsType),
                        ::tolower);
-        if (billionsType == "0.5b") {
-            attention_dropout = 0.0;
-            bos_token_id = 151643;
-            eos_token_id = 151645;
-            std::string hidden_act = "silu";
-            hidden_size = 1024;
-            initializer_range = 0.02;
-            intermediate_size = 2816;
-            max_position_embeddings = 32768;
-            max_window_layers = 21;
-            model_type = "qwen2";
-            num_attention_heads = 16;
-            num_hidden_layers = 24;
-            num_key_value_heads = 16;
-            rms_norm_eps = 1e-6;
-            rope_theta = 1000000.0;
-            sliding_window = 32768;
-            vocab_size = 151936;
-            tie_embedding_words = true;
-        } else if (billionsType == "1.8b") {
-            attention_dropout = 0.0;
-            std::string hidden_act = "silu";
-            hidden_size = 2048;
-            intermediate_size = 5504;
-            max_position_embeddings = 32768;
-            num_attention_heads = 16;
-            num_hidden_layers = 24;
-            num_key_value_heads = 16;
-            rms_norm_eps = 1e-6;
-            rope_theta = 1000000.0;
-            sliding_window = 32768;
-            vocab_size = 151936;
-            tie_embedding_words = false;
-        } else if (billionsType == "1.5b") {
-            attention_dropout = 0.0;
-            std::string hidden_act = "silu";
-            hidden_size = 1536;
-            intermediate_size = 8960;
-            max_position_embeddings = 32768;
-            max_window_layers = 28;
-            num_attention_heads = 12;
-            num_hidden_layers = 28;
-            num_key_value_heads = 2;
-            rms_norm_eps = 1e-6;
-            rope_theta = 1000000.0;
-            sliding_window = 32768;
-            vocab_size = 151936;
-            tie_embedding_words = true;
-        } else if (billionsType == "ds-1.5b") {
+        if (billionsType == "1.5b") {
             attention_dropout = 0.0;
             std::string hidden_act = "silu";
             hidden_size = 1536;
@@ -175,5 +107,3 @@ struct QWenConfig : public TransformerConfig {
     RoPEType RoPE_type = RoPEType::HFHUBROPE;
     QWenNameConfig names_config;
 };
-
-#endif //! CONFIG_QWEN_HPP
