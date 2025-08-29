c0283a4e938102cf827cab2f6faf739acfe99bb7
chenghuaWang <2923277184@qq.com>
2025-08-29 10:53:23 +0800

    refactor: remove unused Qwen2VL example
    - Remove Qwen2VL-related files and configurations
    - Delete entire examples/qwen2_vl directory
    - Remove unused include in mllm/nn/Nn.hpp
    - Clean up elementwise operations in ARM kernels

--- mllm/nn/Nn.hpp
+++ mllm/nn/Nn.hpp
@@ -17,7 +17,6 @@
 #include "mllm/nn/layers/LayerNorm.hpp"       // IWYU pragma: export
 #include "mllm/nn/layers/Softmax.hpp"         // IWYU pragma: export
 #include "mllm/nn/layers/VisionRoPE.hpp"      // IWYU pragma: export
-#include "mllm/nn/layers/Conv1D.hpp"          // IWYU pragma: export
 #include "mllm/nn/layers/Conv3D.hpp"          // IWYU pragma: export
 #include "mllm/nn/layers/CausalMask.hpp"      // IWYU pragma: export
 #include "mllm/nn/layers/MultimodalRoPE.hpp"  // IWYU pragma: export
