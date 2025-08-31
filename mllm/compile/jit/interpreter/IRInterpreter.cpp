c77296d6923695afea13250b16c5055396d21f33
chenghuaWang <2923277184@qq.com>
2025-08-31 14:23:27 +0000

    feat(compile): add IRInterpreter implementation and utils
    - Implement basic structure of IRInterpreter class
    - Add program loading and execution stubs
    - Create IRInterpreterSymbolTable for program management
    - Define ProgramIRInstruction and related payload structures
    - Update OpTypes enum with new operation types
    - Add ScopedRedirect utility for output redirection

--- mllm/compile/jit/interpreter/IRInterpreter.cpp
+++ mllm/compile/jit/interpreter/IRInterpreter.cpp
@@ -3,4 +3,19 @@
 
 #include "mllm/compile/jit/interpreter/IRInterpreter.hpp"
 
-namespace mllm::jit::interpreter {}
+namespace mllm::jit::interpreter {
+
+void IRInterpreter::loadParam(const ParameterFile::ptr_t& parameter_file) {
+  // TODO
+}
+
+void IRInterpreter::loadAndLinkPrograms(const std::string& source_code, const nlohmann::json& program) {
+  // TODO
+}
+
+std::vector<Tensor> IRInterpreter::run(const std::vector<Tensor>& inputs) {
+  // TODO
+  return {};
+}
+
+}  // namespace mllm::jit::interpreter
--- mllm/compile/jit/interpreter/IRInterpreter.hpp
+++ mllm/compile/jit/interpreter/IRInterpreter.hpp
@@ -4,12 +4,44 @@
 
 #include <string>
 #include <vector>
+#include <variant>
+#include <unordered_map>
 #include <nlohmann/json_fwd.hpp>
 
+#include "mllm/compile/ir/program/Op.hpp"
+#include "mllm/core/BaseOp.hpp"
 #include "mllm/core/ParameterFile.hpp"
 #include "mllm/core/Tensor.hpp"
+#include "mllm/compile/ir/GeneratedRTTIKind.hpp"
 
 namespace mllm::jit::interpreter {
+
+using program_id_t = uint64_t;
+
+struct ProgramIRInstructionModeConfigPayload {
+  ir::program::ModeConfigFlag flag;
+};
+
+struct ProgramIRInstructionKernelLaunchPayload {
+  BaseOp::ptr_t mllm_op = nullptr;
+};
+
+struct ProgramIRInstructionJumpPayload {
+  int32_t offset;
+};
+
+struct ProgramIRInstruction {
+  ir::NodeKind program_kind;
+  std::variant<ProgramIRInstructionModeConfigPayload, ProgramIRInstructionKernelLaunchPayload, ProgramIRInstructionJumpPayload>
+      payload;
+};
+
+struct IRInterpreterSymbolTable {
+  std::unordered_map<program_id_t, ProgramIRInstruction> program_id_2_op_table;
+  std::unordered_map<std::string, program_id_t> symbol_name_2_program_id_table;
+};
+
+class IRInterpreterProgram {};
 
 // IRInterpreter Only handle program IR
 class IRInterpreter {
@@ -18,14 +50,14 @@ class IRInterpreter {
 
   ~IRInterpreter() = default;
 
-  void load(const ParameterFile::ptr_t& parameter_file);
+  void loadParam(const ParameterFile::ptr_t& parameter_file);
 
   void loadAndLinkPrograms(const std::string& source_code, const nlohmann::json& program);
 
   std::vector<Tensor> run(const std::vector<Tensor>& inputs);
 
  private:
-  uint64_t program_counter = 0;
+  program_id_t program_counter = 0;
   std::string source_code_;
 };
 
