098d11d085b617ca809cf2914c5c83afa8885bbc
chenghuaWang <2923277184@qq.com>
2025-09-10 22:10:54 +0800

    feat(ffi): add header files for C API and PackedArgs
    - Create C.hpp as the header file for MLLM FFI C API
    - Create PackedArgs.hpp for defining PackedArgs structure

---
+++ mllm/ffi/C.hpp
@@ -0,0 +1,27 @@
+// Copyright (c) MLLM Team.
+// Licensed under the MIT License.
+
+#pragma once
+
+// C API of MLLM FFI.
+
+// #include <dlpack/dlpack.h>
+#include <stdint.h>  // NOLINT
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// TODO
+
+#ifdef __cplusplus
+enum MllmFFITypeID : int32_t {
+#else
+typedef enum {
+#endif
+
+};
+
+#ifdef __cplusplus
+}
+#endif
