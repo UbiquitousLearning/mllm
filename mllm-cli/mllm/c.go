ae857408d35e2f8b65ff4316d237ebc46ef64238
chenghuaWang <2923277184@qq.com>
2025-09-15 21:34:39 +0800

    build: update code style and remove unused C bindings
    - Update .editorconfig to add Go file formatting
    - Remove unused C bindings and SDK import from mllm/c.go

--- mllm-cli/mllm/c.go
+++ mllm-cli/mllm/c.go
@@ -3,25 +3,11 @@
 
 package mllm
 
-/*
-#cgo CFLAGS: -fPIC -I${SRCDIR}/SDK/include/
-#cgo CFLAGS: -std=c11
-#cgo LDFLAGS: -L${SRCDIR}/SDK/lib/
-#cgo LDFLAGS: -lMllmSdkC
-#cgo LDFLAGS: -Wl,-rpath ${SRCDIR}/SDK/lib
-
-#include <mllm/mllm-c.hpp>
-#include <stdlib.h>
-*/
 import "C"
 
 func InitContext() {
-	ret := C.mllm_ret_is_success(C.mllm_init_context())
-	if ret != 0 {
-		panic("Failed to initialize MLLM context")
-	}
+
 }
 
 func ShutdownContext() {
-	C.mllm_shutdown_context()
 }
