1c0aa46dc516b9802f4d9d1779108441fb5e3478
oreomaker <zh002919@outlook.com>
2025-09-15 16:42:08 +0800

    chore(examples): add minicpm_o empty main file

---
+++ examples/minicpm_o/main.cpp
@@ -0,0 +1,16 @@
+
+#include <fmt/core.h>
+#include <cstdint>
+#include <cstdlib>
+#include <mllm/mllm.hpp>
+#include "mllm/core/DataTypes.hpp"
+#include "mllm/core/ParameterFile.hpp"
+#include "mllm/core/Tensor.hpp"
+#include "mllm/models/minicpm_o2_6/configuration_chattts.hpp"
+#include "mllm/models/minicpm_o2_6/modeling_chattts.hpp"
+#include "mllm/utils/Common.hpp"
+#include <fstream>
+
+using namespace mllm;  // NOLINT
+
+MLLM_MAIN({NYI("leave empty")});
