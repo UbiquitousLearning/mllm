389d6284bd582b45dc4c74b5f099775898ac861f
chenghuaWang <2923277184@qq.com>
2025-10-16 08:37:32 +0000

    feat(ffi): add Nn.cc and Compile.cc to FFI extension
    Added new source files Nn.cc and Compile.cc to the MllmFFIExtension library
    in CMakeLists.txt to extend the FFI interface.
    
    feat(build): format MLIR installation script
    
    Reformatted the cmake command in install_mlir.sh to a single line for better
    readability and consistency in the build script.

--- scripts/install_mlir.sh
+++ scripts/install_mlir.sh
@@ -2,14 +2,5 @@ cd ..
 git clone https://github.com/llvm/llvm-project.git
 mkdir llvm-project/build
 cd llvm-project/build
-cmake -G Ninja ../llvm \
-   -DLLVM_ENABLE_PROJECTS=mlir \
-   -DLLVM_BUILD_EXAMPLES=ON \
-   -DLLVM_TARGETS_TO_BUILD="Native;NVPTX" \
-   -DCMAKE_BUILD_TYPE=Release \
-   -DLLVM_ENABLE_ASSERTIONS=ON
-   -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_LLD=ON \
-   -DLLVM_CCACHE_BUILD=ON \
-   -DLLVM_USE_SANITIZER="Address;Undefined" \
-   -DMLIR_INCLUDE_INTEGRATION_TESTS=ON
+cmake -G Ninja ../llvm -DLLVM_ENABLE_PROJECTS=mlir -DLLVM_BUILD_EXAMPLES=ON -DLLVM_TARGETS_TO_BUILD="Native;NVPTX" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_LLD=ON -DLLVM_CCACHE_BUILD=ON -DLLVM_USE_SANITIZER="Address;Undefined" -DMLIR_INCLUDE_INTEGRATION_TESTS=ON 
 cmake --build . --target check-mlir
