9c8d9daec149390720962fa93a41c43d7a18432a
chenghuaWang <2923277184@qq.com>
2025-11-19 10:44:36 +0800

    feat(thread-pool): implement HpcThreadPool for efficient CPU task management and update build configurations

--- mllm/backends/cpu/CPUBackend.hpp
+++ mllm/backends/cpu/CPUBackend.hpp
@@ -5,13 +5,30 @@
 
 #include <memory>
 #include "mllm/backends/base/Backend.hpp"
+#include "mllm/engine/HpcThreadPool.hpp"
 
 namespace mllm::cpu {
 
 class CPUBackend final : public Backend {
  public:
+  ~CPUBackend();
+
   CPUBackend();
+
+  HpcThreadPool::ptr_t getThreadPool();
+
+  void initThreadPool(int32_t num_threads);
+
+  int32_t taskIndex();
+
+ private:
+  int32_t task_index_ = -1;
+  HpcThreadPool::ptr_t thread_pool_ = nullptr;
 };
 
 std::shared_ptr<CPUBackend> createCPUBackend();
+
+void idleHpcThreadPool();
+
+void wakeupHpcThreadPool();
 }  // namespace mllm::cpu
