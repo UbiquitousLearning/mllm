70d98a9c97e8b94c14bf7febf6b287ffe1d38403
Your Name <you@example.com>
2025-12-12 01:08:03 +0800

    feat(ascend memory): introduce memory pool to Ascend backend

---
+++ mllm/backends/ascend/memory/AscendMemoryManager.cpp
@@ -0,0 +1,67 @@
+// Copyright (c) MLLM Team.
+// Licensed under the MIT License.
+
+#include <acl/acl.h>
+
+#include "AscendMemoryManager.hpp"
+#include "mllm/backends/ascend/AscendCommon.hpp"
+#include "mllm/utils/Common.hpp"
+#include "mllm/utils/Log.hpp"
+
+namespace mllm::ascend {
+
+static AscendMemoryManager g_ascendMemoryManager;
+
+AscendMemoryManager::AscendMemoryManager() {}
+
+AscendMemoryManager &getAscendMemoryManager() {
+    return g_ascendMemoryManager;
+}
+
+void AscendMemoryManager::createMemoryPool(size_t pool_size)
+{
+    uint32_t device_count = 0;
+    auto ret = aclrtGetDeviceCount(&device_count);
+    MLLM_ACL_CHECK(ret);
+    for (size_t i = 0; i < device_count; i++) {
+
+        aclrtSetDevice(i);
+
+        std::shared_ptr<AscendMemoryPool> memory_pool = std::make_shared<AscendMemoryPool>(pool_size);
+        memory_pools_.push_back(memory_pool);
+        MLLM_INFO("create mempool for device {} success", i);
+    }
+}
+
+int32_t AscendMemoryManager::getDeviceId()
+{
+    int32_t device_id = -1;
+    auto ret = aclrtGetDevice(&device_id);
+    MLLM_ACL_CHECK(ret);
+    return device_id;
+}
+
+std::shared_ptr<AscendMemoryPool> &AscendMemoryManager::getMemoryPool()
+{
+    size_t device_id = static_cast<size_t>(getDeviceId());
+    if (device_id >= memory_pools_.size()) {
+        MLLM_ERROR_EXIT(::mllm::ExitCode::kAscendError, "Invalid device id {}", device_id);
+    }
+    return memory_pools_[device_id];
+}
+
+void AscendMemoryManager::allocateBlock(uint32_t size, int &block_id)
+{
+    getMemoryPool()->allocateBlock(size, block_id);
+}
+
+void AscendMemoryManager::freeBlock(int block_id)
+{
+    getMemoryPool()->freeBlock(block_id);
+}
+
+void AscendMemoryManager::getBlockPtr(int block_id, void *&addr)
+{
+    getMemoryPool()->getBlockPtr(block_id, addr);
+}
+}  // namespace mllm::ascend
---
+++ mllm/backends/ascend/memory/AscendMemoryPool.hpp
@@ -0,0 +1,40 @@
+// Copyright (c) MLLM Team.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include <atomic>
+#include <mutex>
+#include <unordered_map>
+#include <cstdint>
+#include <cstddef>
+#include "MemoryBlock.hpp"
+
+namespace mllm::ascend {
+
+class AscendMemoryPool {
+public:
+    explicit AscendMemoryPool(size_t pool_size);
+    ~AscendMemoryPool();
+
+    void allocateBlock(uint32_t size, int& block_id);
+
+    void freeBlock(int block_id);
+
+    void getBlockPtr(int block_id, void*& addr);
+
+private:
+    uint64_t generateBlocksId();
+
+    std::atomic<uint64_t> id_ = 0;  
+    std::mutex block_mutex_;              
+    
+    void* base_mem_ptr_ = nullptr;      
+    void* cur_mem_ptr_ = nullptr;   
+    int64_t remain_size_ = 0;       
+    
+    std::unordered_map<int, MemoryBlock> used_blocks_;  
+    std::unordered_map<int, MemoryBlock> free_blocks_;  
+};
+
+}  // namespace mllm::ascend
