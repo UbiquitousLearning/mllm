7ccb33072534a294b5a8434781b3b109c6d8e4b9
LucyLiu <15015773+lucyliu125@user.noreply.gitee.com>
2026-01-14 17:21:35 +0800

    qnn_aot_env.py针对x86的改进

--- pymllm/backends/qualcomm/qnn_aot_env.py
+++ pymllm/backends/qualcomm/qnn_aot_env.py
@@ -1,9 +1,21 @@
-from pymllm.ffi import (
-    QnnDeviceAndContext,
-    QnnAOTEnv,
-    QcomChipset,
-    QcomHTPArch,
-    QcomSecurityPDSession,
-    QcomTargetMachine,
-    QcomTryBestPerformance,
-)
+from pymllm.ffi import is_qnn_aot_on_x86_enabled
+
+if is_qnn_aot_on_x86_enabled():
+    from pymllm.ffi import (
+        QnnDeviceAndContext,
+        QnnAOTEnv,
+        QcomChipset,
+        QcomHTPArch,
+        QcomSecurityPDSession,
+        QcomTargetMachine,
+        QcomTryBestPerformance,
+    )
+else:
+    # Define placeholder classes when QNN AOT is not enabled
+    QnnDeviceAndContext = None
+    QnnAOTEnv = None
+    QcomChipset = None
+    QcomHTPArch = None
+    QcomSecurityPDSession = None
+    QcomTargetMachine = None
+    QcomTryBestPerformance = None
