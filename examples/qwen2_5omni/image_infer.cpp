c4f230648ccce567ced2d7c8990d81757a64aa24
KKkai <1640576073@qq.com>
2026-01-23 15:35:15 +0800

    fix

--- examples/qwen2_5omni/image_infer.cpp
+++ examples/qwen2_5omni/image_infer.cpp
@@ -50,12 +50,12 @@ MLLM_MAIN({
     std::string prompt_text;
 
     fmt::print("Image path (or 'exit/quit'): ");
-    image_path = "../../../mllm2-former/mllm/rsc/pics.jpg";
+    image_path = "";
     //std::getline(std::cin, image_path);
     if (image_path == "exit" || image_path == "quit") { return 0; }
 
     fmt::print("Prompt text: ");
-    prompt_text = "描述图片中物体";
+    prompt_text = "";
     //std::getline(std::cin, prompt_text);
 
     try {
